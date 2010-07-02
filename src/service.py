b1542efeab86d48dcd619585fbae6c376d2e653f
Gabes Jean <naparuba@users.sourceforge.net>
2010-07-02 10:45:22 +0200

    Add check_period implicitly inheritate to service from host.

--- src/service.py
+++ src/service.py
@@ -697,7 +697,7 @@ class Services(Items):
     def apply_implicit_inheritance(self, hosts):
         for prop in ['contacts', 'contact_groups', 'notification_interval', \
                          'notification_period', 'resultmodulations', 'escalations', \
-                         'poller_tag']:
+                         'poller_tag', 'check_period']:
             for s in self:
                 if not s.is_tpl():
                     if not hasattr(s, prop) and hasattr(s, 'host_name'):
