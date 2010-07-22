5a39dfe9e2d7dad6f880376a6a218f3c860cf800
Gabes Jean <naparuba@users.sourceforge.net>
2010-07-22 15:46:26 +0200

    Remove unused tests.

--- test/pyro_client_ns.py
+++
@@ -1,7 +0,0 @@
-import Pyro.core
-
-# finds object automatically if you're running the Name Server.
-jokes = Pyro.core.getProxyForURI("PYRONAME://jokegen")
-
-print jokes.joke("Irmen")
-
--- test/test.sh
+++ test/test.sh
@@ -2,4 +2,5 @@
 
 cd test
 echo `pwd`
-python test_problem_impact.py
+#python test_problem_impact.py
+for ii in `ls -1 test_*py`; do echo "Launching Test $ii" && python $ii; done
