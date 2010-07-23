4a947b3fd29214ca15cd8359455f7b9c9444099d
Gabes Jean <naparuba@users.sourceforge.net>
2010-07-23 14:37:09 +0200

    Add a funky hard multi daterange exclusions test.(and it works great ;) )

--- test/test_timeperiods.py
+++ test/test_timeperiods.py
@@ -231,6 +231,55 @@ class TestTimeperiods(ShinkenTest):
         
         self.assert_(t_next == 'Tue Aug 31 16:30:00 2010')
 
+
+
+    def test_funky_mondayweek_timeperiod_with_exclude_and_multiple_daterange(self):
+        self.print_header()
+        now = time.time()
+        #Get the 12 of july 2010 at 15:00, monday
+        july_the_12 = time.mktime(time.strptime("12 Jul 2010 15:00:00", "%d %b %Y %H:%M:%S"))
+
+        #Then a funny daterange
+        print "Testing daterange", 'tuesday -1 - monday 1  16:30-24:00'
+        t = Timeperiod()
+        t.timeperiod_name = 'T1'
+        t.resolve_daterange(t.dateranges, 'tuesday -1 - monday 1  16:30-24:00')
+        t_next = t.get_next_valid_time_from_t(july_the_12)
+        t_next = time.asctime(time.localtime(t_next))
+        print "Next without exclude", t_next
+        self.assert_(t_next == "Tue Jul 27 16:30:00 2010")
+
+        #Now we add this timeperiod an exception
+        #And a good one : from april (so before so agust (after), and full time.
+        #But the 27 is nw not possible? So what next? Add a month! 
+        #But maybe it's not enoutgth? :)
+        #The withoutthe 2nd exclude, it's the Tues Aug 31, btu it's inside
+        #saturday -1 - monday 1 because saturday -1 is the 28 august, so no.
+        #in september saturday -1 is the 25, and tuesday -1 is 28, so still no
+        #A month again! So now tuesday -1 is 26 and saturday -1 is 30. So ok
+        #for this one! that was quite long isn't it? And funky ! :)
+        t2 = Timeperiod()
+        t2.timeperiod_name = 'T2'
+        t2.resolve_daterange(t2.dateranges, 'april 1 - august 16 00:00-24:00')
+        #Oups, I add a inner daterange ;)
+        t2.resolve_daterange(t2.dateranges, 'saturday -1 - monday 1  16:00-24:00')
+        t.exclude = [t2]
+        #We are a bad boy : first time period want a tuesday
+        #but exclude do not want it until 23:58. So next is 59 :)
+        t.cache = {}
+        t_next = t.get_next_valid_time_from_t(july_the_12)
+        #print "Check from", time.asctime(time.localtime(july_the_12))
+        #t_exclude = t2.get_next_valid_time_from_t(july_the_12)
+        t_exclude_inv = t2.get_next_invalid_time_from_t(july_the_12)
+        #print "T2 next valid", time.asctime(time.localtime(t_exclude))
+        print "Next invalid T2", time.asctime(time.localtime(t_exclude_inv))
+        
+        print "T next raw", t_next
+        print "T next?", time.asctime(time.localtime(t_next))
+        t_next = time.asctime(time.localtime(t_next))
+        
+        self.assert_(t_next == 'Tue Oct 26 16:30:00 2010')
+        print "Finish this Funky test :)"
         
 
 
