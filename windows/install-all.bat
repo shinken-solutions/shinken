4fefd766acc6e3ac429a5e55255e4a309efe5a04
Gabes Jean <naparuba@users.sourceforge.net>
2010-07-29 11:18:32 +0200

    fix: forgot some windows installation related files.

---
+++ windows/install-all.bat
@@ -0,0 +1,9 @@
+c:\shinken\src\windows\instsrv.exe "Shinken-Arbiter" "c:\shinken\src\windows\srvany.exe"
+
+c:\shinken\src\windows\instsrv.exe "Shinken-Scheduler" "c:\shinken\src\windows\srvany.exe"
+
+c:\shinken\src\windows\instsrv.exe "Shinken-Poller" "c:\shinken\src\windows\srvany.exe"
+
+c:\shinken\src\windows\instsrv.exe "Shinken-Reactionner" "c:\shinken\src\windows\srvany.exe"
+
+c:\shinken\src\windows\instsrv.exe "Shinken-Broker" "c:\shinken\src\windows\srvany.exe"
