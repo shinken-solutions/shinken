72f219232b521ff211809d210aab8eba59865b0d
Gabes Jean <naparuba@users.sourceforge.net>
2010-07-29 15:36:32 +0200

    Update the doc and the windows files for the new layout.

--- windows/install-all.bat
+++ windows/install-all.bat
@@ -1,9 +1,9 @@
-c:\shinken\src\windows\instsrv.exe "Shinken-Arbiter" "c:\shinken\src\windows\srvany.exe"
+c:\shinken\windows\instsrv.exe "Shinken-Arbiter" "c:\shinken\windows\srvany.exe"
 
-c:\shinken\src\windows\instsrv.exe "Shinken-Scheduler" "c:\shinken\src\windows\srvany.exe"
+c:\shinken\windows\instsrv.exe "Shinken-Scheduler" "c:\shinken\windows\srvany.exe"
 
-c:\shinken\src\windows\instsrv.exe "Shinken-Poller" "c:\shinken\src\windows\srvany.exe"
+c:\shinken\windows\instsrv.exe "Shinken-Poller" "c:\shinken\windows\srvany.exe"
 
-c:\shinken\src\windows\instsrv.exe "Shinken-Reactionner" "c:\shinken\src\windows\srvany.exe"
+c:\shinken\windows\instsrv.exe "Shinken-Reactionner" "c:\shinken\windows\srvany.exe"
 
-c:\shinken\src\windows\instsrv.exe "Shinken-Broker" "c:\shinken\src\windows\srvany.exe"
+c:\shinken\windows\instsrv.exe "Shinken-Broker" "c:\shinken\windows\srvany.exe"
