307ba492557840bf58c59493c315139a09b24bef
Gabes Jean <naparuba@users.sourceforge.net>
2010-07-30 16:39:52 +0200

    Very important code. ....                                                 just for fun.

---
+++ shinken/easter.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+#Copyright (C) 2009-2010 : 
+#    Gabes Jean, naparuba@gmail.com 
+#    Gerhard Lausser, Gerhard.Lausser@consol.de
+#
+#This file is part of Shinken.
+#
+#Shinken is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Shinken is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+#
+#You should have received a copy of the GNU Affero General Public License
+#along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def dark():
+    r"""
+                       .-.
+                      |_:_|
+                     /(_Y_)\
+                    ( \/M\/ )
+ '.               _.'-/'-'\-'._
+   ':           _/.--'[[[[]'--.\_
+     ':        /_'  : |::"| :  '.\
+       ':     //   ./ |oUU| \.'  :\
+         ':  _:'..' \_|___|_/ :   :|
+           ':.  .'  |_[___]_|  :.':\
+            [::\ |  :  | |  :   ; : \
+             '-'   \/'.| |.' \  .;.' |
+             |\_    \  '-'   :       |
+             |  \    \ .:    :   |   |
+             |   \    | '.   :    \  |
+             /       \   :. .;       |
+            /     |   |  :__/     :  \\
+           |  |   |    \:   | \   |   ||
+          /    \  : :  |:   /  |__|   /|
+      snd |     : : :_/_|  /'._\  '--|_\
+          /___.-/_|-'   \  \
+                         '-'
+
+"""
+    print dark.__doc__
+
+
+
+
+def episode_iv():
+    hst = 'towel.blinkenlights.nl'
+
+    from telnetlib import Telnet
+
+    t = Telnet(hst)
+    while True:
+        buf = t.read_until('mesfesses', 0.1)
+        print buf
+
+
+def perdu():
+    import urllib
+    f = urllib.urlopen("http://www.perdu.com")
+    print f.read()
+
+
+
+def myip():
+    import urllib
+    f = urllib.urlopen("http://whatismyip.org/")
+    print f.read()
+
+
+
