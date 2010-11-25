f4ba2eeb46a3c48f1b7b96fa6dfcd032dffb25b8
Gabes Jean <naparuba@users.sourceforge.net>
2010-11-25 16:00:17 +0100

    *Fix: now the getstate/setstate for host/service is fully compliant with adding properties. Less beter for speed far better for retention!

--- shinken/service.py
+++ shinken/service.py
@@ -269,21 +269,20 @@ class Service(SchedulingItem):
 
 
     #Call by picle for dataify service
-    #Here we do not want a dict, it's too heavy
-    #We create a list with properties inlined
+    #we do a dict because list are too dangerous for
+    #retention save and co :( even if it's more
+    #extensive
     #The setstate function do the inverse
     def __getstate__(self):
 #        print "Asking a getstate for service", self.get_dbg_name()
         cls = self.__class__
         #id is not in *_properties
-        res = [self.id]
+        res = {'id' : self.id}
         for prop in cls.properties:
-            res.append(getattr(self, prop))
+            res[prop] = getattr(self, prop)
         for prop in cls.running_properties:
-            res.append(getattr(self, prop))
-        #We reverse because we want to recreate
-        #By check at properties in the same order
-        res.reverse()
+            res[prop] = getattr(self, prop)
+
         return res
 
 
@@ -291,12 +290,11 @@ class Service(SchedulingItem):
     def __setstate__(self, state):
 #        print "Set a service state", state['host_name'], state['service_description']
         cls = self.__class__
-        self.id = state.pop()
+        self.id = state['id']
         for prop in cls.properties:
-	    val = state.pop()
-            setattr(self, prop, val)
+            setattr(self, prop, state[prop])
         for prop in cls.running_properties:
-            setattr(self, prop, state.pop())
+            setattr(self, prop, state[prop])
 
 
     #Check is required prop are set:
