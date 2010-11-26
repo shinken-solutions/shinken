44be4126e1802e6ff618451c309dfcd411c8f7bb
Gabes Jean <naparuba@users.sourceforge.net>
2010-11-26 13:11:17 +0100

    Add: pylint and noseets pass for unit tests and code quality.

---
+++ test/nose_pass.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#Copyright (C) 2009-2010 :
+#    Gabes Jean, naparuba@gmail.com
+#    Gerhard Lausser, Gerhard.Lausser@consol.de
+#
+#This file is part of Shinken.
+#
+#Shinken is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Shinken is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+#
+#You should have received a copy of the GNU Affero General Public License
+#along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
+
+
+DIR=$(cd $(dirname "$0"); pwd)
+cd $DIR
+echo `pwd`
+
+echo "Launching coverage pass"
+
+nosetests -v -s --with-xunit --with-coverage --cover-erase *py
+
+echo "Coverage pass done, congrats or not? ;) "
+
