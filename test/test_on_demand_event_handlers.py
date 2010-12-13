64f3d591113084b31a1ab1b5ccdb326dcfe41fd6
Gabes Jean <naparuba@users.sourceforge.net>
2010-12-13 16:49:09 +0100

    Fix: forgot the add of on demande event handler tests.

---
+++ test/test_on_demand_event_handlers.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python2.6
+#Copyright (C) 2009-2010 :
+#    Gabes Jean, naparuba@gmail.com
+#    Gerhard Lausser, Gerhard.Lausser@consol.de
+#
+#This file is part of Shinken.
+#
+#Shinken is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Shinken is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+#
+#You should have received a copy of the GNU Affero General Public License
+#along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+# This file is used to test acknowledge of problems
+#
+
+
+#It's ugly I know....
+from shinken_test import *
+
+class TestConfig(ShinkenTest):
+
+    def setUp(self):
+        self.setup_with_file('etc/nagios_on_demand_event_handlers.cfg')
+
+
+    def test_on_demand_eh(self):
+        self.print_header()
+        # retry_interval 2
+        # critical notification
+        # run loop -> another notification
+        now = time.time()
+        host = self.sched.hosts.find_by_name("test_host_0")
+        host.checks_in_progress = []
+        host.act_depend_of = [] # ignore the router
+        svc = self.sched.services.find_srv_by_name_and_hostname("test_host_0", "test_ok_0")
+        svc.checks_in_progress = []
+        svc.act_depend_of = [] # no hostchecks on critical checkresults
+        #--------------------------------------------------------------
+        # initialize host/service state
+        #--------------------------------------------------------------
+        self.assert_(svc.automatic_event_handler == False)
+
+        self.scheduler_loop(5, [[svc, 2, 'CRITICAL']])
+        # We should NOT see any event hnalder here :)
+        self.assert_(not self.any_log_match('SERVICE EVENT HANDLER'))
+        print "MY Actions", self.sched.actions
+        
+        # And now we ask for a launch in manual
+        now = time.time()
+        cmd = "[%lu] LAUNCH_SVC_EVENT_HANDLER;test_host_0;test_ok_0" % now
+        self.sched.run_external_command(cmd)
+        self.sched.get_new_actions()
+        self.worker_loop()
+        self.assert_(self.any_log_match('SERVICE EVENT HANDLER'))
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
