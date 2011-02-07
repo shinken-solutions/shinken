25699e6082fb19ef881dc91f8bed7997c3a01803
Gabes Jean <naparuba@users.sourceforge.net>
2011-02-07 10:24:43 +0100

    *Fix : bug in arbtier for bad configuration in is_correct *Add : hack command poller tag module

---
+++ test/test_module_hack_cmd_poller_tag.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python2.6
+#Copyright (C) 2009-2010 :
+#    Gabes Jean, naparuba@gmail.com
+#    Gerhard Lausser, Gerhard.Lausser@consol.de
+#
+#This file is part of Shinken.
+#
+#Shinken is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Shinken is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+#
+#You should have received a copy of the GNU Affero General Public License
+#along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#
+# This file is used to test reading and processing of config files
+#
+
+import os
+
+from shinken_test import unittest, ShinkenTest
+
+from shinken.log import logger
+from shinken.objects.module import Module
+from shinken.modules import hack_commands_poller_tag_arbiter
+from shinken.modules.hack_commands_poller_tag_arbiter import get_instance 
+
+
+
+class TestHackCmdPollerTag(ShinkenTest):
+    #setUp is in shinken_test
+    def setUp(self):
+        self.setup_with_file('etc/nagios_module_hack_cmd_poller_tag.cfg')
+
+
+    #Change ME :)
+    def test_hack_cmd_poller_tag(self):
+        modconf = self.conf.modules.find_by_name('HackCommandsPollerTag')
+
+        #get our modules
+        mod = hack_commands_poller_tag_arbiter.Hack_cmds_pt(modconf)
+        # Look if we really change our commands
+
+        # Calls the mod with our config
+        mod.hook_late_configuration(self)
+        
+        cmd1 = self.sched.commands.find_cmd_by_name('should_change')
+        self.assert_(cmd1 != None)
+        cmd2 = self.sched.commands.find_cmd_by_name('should_not_change')
+        self.assert_(cmd2 != None)
+        
+        print "CMD1", cmd1.__dict__
+        print "P", cmd1.poller_tag
+        self.assert_(cmd1.poller_tag == 'other')
+        print "CMD2", cmd2.__dict__
+        print "P", cmd1.poller_tag
+        self.assert_(cmd2.poller_tag == 'other')
+        
+        # look for a service that use it
+        h1 = self.sched.hosts.find_by_name("test_host_0")
+        self.assert_(h1 != None)
+        h2 = self.sched.hosts.find_by_name("test_router_0")
+        self.assert_(h2 != None)
+        
+        print "H1", h1.check_command
+        print h1.check_command.command
+        self.assert_(h1.check_command.poller_tag == 'other')
+        print "H2", h2.check_command
+        print h2.check_command.command
+        self.assert_(h2.check_command.poller_tag == 'other')
+        
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
