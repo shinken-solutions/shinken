e024c4624006930c14e4783fc185bf9378f069d9
Grégory Starck <g.starck@gmail.com>
2011-02-16 18:11:21 +0100

    Add/Fix: set default encoding to UTF-8 for every shinken daemon.
    Was previously making encoding/decoding errors to appear in lots of place.

--- shinken/bin.py
+++ shinken/bin.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #Copyright (C) 2009-2010 :
 #    Gabes Jean, naparuba@gmail.com
 #    Gerhard Lausser, Gerhard.Lausser@consol.de
@@ -28,6 +28,11 @@ Shinken.
 """
 
 import sys
+reload(sys)
+
+sys.setdefaultencoding('UTF-8')
+
+
 
 VERSION = "0.5+"
 
