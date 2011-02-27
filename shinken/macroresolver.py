shinken-solutions/shinken

commit 08f30ec21347378e3d72b1ca45919504f767db63

Changing `xx ==/!= None` into `xx is (not) None` as suggested by POP 8.

shinken/macroresolver.py
@@ -187,7 +187,7 @@ class MacroResolver(Borg):
             #Now we get values from elements
             for macro in macros:
                 #If type ARGN, look at ARGN cutting
-                if macros[macro]['type'] == 'ARGN' and args!= None:
+                if macros[macro]['type'] == 'ARGN' and argsis not None:
                     macros[macro]['val'] = self.resolve_argn(macro, args)
                     macros[macro]['type'] = 'resolved'
                 #If class, get value from properties
@@ -305,7 +305,7 @@ class MacroResolver(Borg):
                         host_name = elt.host_name
             #Okn now we get service
             s = self.services.find_srv_by_name_and_hostname(host_name, service_description)
-            if s != None:
+            if s is not None:
                 cls = s.__class__
                 prop = cls.macros[macro_name]
                 val = self.get_value_from_element(s, prop)
@@ -327,7 +327,7 @@ class MacroResolver(Borg):
                 if macro_name in cls.macros:
                     prop = cls.macros[macro_name]
                     i = list.find_by_name(elt_name)
-                    if i != None:
+                    if i is not None:
                         val = self.get_value_from_element(i, prop)
             #print "Got val:", val
             return val

shinken/objects/timeperiod.py
@@ -212,7 +212,7 @@ class Timeperiod(Item):
 
             #Min but not the None valus...
             try:
-                local_min = min([d for d in dr_mins if d!=None])
+                local_min = min([d for d in dr_mins if dis notNone])
             except ValueError: #dr_mins if full of None, not good
                 local_min = None
 
@@ -225,7 +225,7 @@ class Timeperiod(Item):
             still_loop = False
 
             #if we've got a real value, we check it with the exclude
-            if local_min != None:
+            if local_min is not None:
                 #Now check if local_min is not valid
                 for tp in self.exclude:
                     #print self.get_name(), "Check in TP"
@@ -242,7 +242,7 @@ class Timeperiod(Item):
                     #else:
                     #    print self.get_name(), "Tp ca lui va", tp.get_name()
 
-            if local_min == None:
+            if local_min is None:
                 still_loop = False
             else:
                 t = local_min
@@ -297,7 +297,7 @@ class Timeperiod(Item):
                 m = dr.get_next_invalid_time_from_t(local_min)
 
                 #print self.get_name(), "Dr give me next invalid", time.asctime(time.localtime(m))
-                if m != None:
+                if m is not None:
                     #But maybe it's invalid for this dr, but valid for other ones.
                     if not self.is_time_valid(m):
                     #print "Final : Got a next invalid at", time.asctime(time.localtime(m))
@@ -316,7 +316,7 @@ class Timeperiod(Item):
             still_loop = False
 
             #if we've got a real value, we check it with the exclude
-            if local_min != None:
+            if local_min is not None:
                 #Now check if local_min is not valid
                 for tp in self.exclude:
                     #print self.get_name(),"we check for invalid", time.asctime(time.localtime(local_min)), 'with tp', tp.name
@@ -331,7 +331,7 @@ class Timeperiod(Item):
 
             if not still_loop:#We find a possible value
                 #We take the result the minimal possible
-                if res == None or local_min < res:
+                if res is None or local_min < res:
                     res = local_min
 
         #print "Finished Return the next invalid", time.asctime(time.localtime(local_min))
@@ -575,7 +575,7 @@ class Timeperiod(Item):
             #print "I will exclude from:", excluded_tps
             for tp_name in excluded_tps:
                 tp = timeperiods.find_by_name(tp_name.strip())
-                if tp != None:
+                if tp is not None:
                     new_exclude.append(tp)
                 else:
                     print "Error : the timeperiod", tp_name, "is unknown!"

shinken/schedulerlink.py
@@ -68,7 +68,7 @@ class SchedulerLink(SatelliteLink):
 
 
     def run_external_command(self, command):
-        if self.con == None:
+        if self.con is None:
             self.create_connexion()
         if not self.alive:
             return None
@@ -94,7 +94,7 @@ class SchedulerLink(SatelliteLink):
 
 
     def give_satellite_cfg(self):
-        return {'port' : self.port, 'address' : self.address, 'name' : self.scheduler_name, 'instance_id' : self.id, 'active' : self.conf!=None}
+        return {'port' : self.port, 'address' : self.address, 'name' : self.scheduler_name, 'instance_id' : self.id, 'active' : self.confis notNone}
 
 
     #Some parameters can give as 'overriden parameters' like use_timezone

shinken/arbiterlink.py
@@ -81,7 +81,7 @@ class ArbiterLink(SatelliteLink):
 
 
     def do_not_run(self):
-        if self.con == None:
+        if self.con is None:
             self.create_connexion()
         try:
             self.con.do_not_run()
@@ -110,7 +110,7 @@ class ArbiterLinks(SatelliteLinks):
             new_modules = []
             for plug_name in s.modules:
                 plug = modules.find_by_name(plug_name.strip())
-                if plug != None:
+                if plug is not None:
                     new_modules.append(plug)
                 else:
                     print "Error : the module %s is unknow for %s" % (plug_name, s.get_name())

shinken/check.py
@@ -57,7 +57,7 @@ class Check(Action):
 
         self.is_a = 'check'
         self.type = ''
-        if id == None: #id != None is for copy call only
+        if id is None: #id != None is for copy call only
             self.id = Action.id
             Action.id += 1
         self._in_timeout = False

shinken/daemon.py
@@ -165,7 +165,7 @@ class Daemon(object):
  
     def load_config_file(self):
         self.parse_config_file()
-        if self.config_file != None:
+        if self.config_file is not None:
             # Some paths can be relatives. We must have a full path by taking
             # the config file by reference
             self.relative_paths_to_full(os.path.dirname(self.config_file))
@@ -449,7 +449,7 @@ If change failed we sys.exit(2) """
 
         uid = self.find_uid_from_name()
         gid = self.find_gid_from_name()
-        if uid == None or gid == None:
+        if uid is None or gid is None:
             print "Exiting"
             sys.exit(2)
         try:
@@ -467,14 +467,14 @@ If change failed we sys.exit(2) """
 If some properties need a pythonization, we do it.
 Also put default value in the properties if some are missing in the config_file """
         properties = self.__class__.properties
-        if self.config_file != None:
+        if self.config_file is not None:
             config = ConfigParser.ConfigParser()
             config.read(self.config_file)
             if config._sections == {}:
                 print "Bad or missing config file : %s " % self.config_file
                 sys.exit(2)
             for (key, value) in config.items('daemon'):
-                if key in properties and properties[key]['pythonize'] != None:
+                if key in properties and properties[key]['pythonize'] is not None:
                     value = properties[key]['pythonize'](value)
                 setattr(self, key, value)
         else:
@@ -483,7 +483,7 @@ Also put default value in the properties if some are missing in the config_file 
         for prop in properties:
             if not hasattr(self, prop):
                 value = properties[prop]['default']
-                if prop in properties and properties[prop]['pythonize'] != None:
+                if prop in properties and properties[prop]['pythonize'] is not None:
                     value = properties[prop]['pythonize'](value)
                 setattr(self, prop, value)
                 print "Using default value :", prop, value

shinken/daemons/brokerdaemon.py
@@ -121,7 +121,7 @@ class Broker(BaseSatellite):
     def pynag_con_init(self, id, type='scheduler'):
             # Get teh good links tab for looping..
         links = self.get_links_from_type(type)
-        if links == None:
+        if links is None:
             logger.log('DBG: Type unknown for connexion! %s' % type)
             return
 
@@ -239,7 +239,7 @@ class Broker(BaseSatellite):
     def get_new_broks(self, type='scheduler'):
             # Get teh good links tab for looping..
         links = self.get_links_from_type(type)
-        if links == None:
+        if links is None:
             logger.log('DBG: Type unknown for connexion! %s' % type)
             return
 

shinken/daemons/schedulerdaemon.py
@@ -179,7 +179,7 @@ class Shinken(BaseSatellite):
                 new_t = ref.check_period.get_next_valid_time_from_t(new_t)
                 # But maybe no there is no more new value! Not good :(
                 # Say as error, with error output
-                if new_t == None:
+                if new_t is None:
                     c.state = 'waitconsume'
                     c.exit_status = 2
                     c.output = '(Error: there is no available check time after time change!)'
@@ -208,7 +208,7 @@ class Shinken(BaseSatellite):
 
                 # But maybe no there is no more new value! Not good :(
                 # Say as error, with error output
-                if new_t == None:
+                if new_t is None:
                     c.state = 'waitconsume'
                     c.exit_status = 2
                     c.output = '(Error: there is no available check time after time change!)'
@@ -280,7 +280,7 @@ class Shinken(BaseSatellite):
         
         # give it an interface
         # But first remove previous interface if exists
-        if self.ichecks != None:
+        if self.ichecks is not None:
             print "Deconnecting previous Check Interface from pyro_daemon"
             self.pyro_daemon.unregister(self.ichecks.pyro_obj)
         #Now create and connect it
@@ -289,7 +289,7 @@ class Shinken(BaseSatellite):
         print "The Checks Interface uri is:", self.uri
 
         #Same for Broks
-        if self.ibroks != None:
+        if self.ibroks is not None:
             print "Deconnecting previous Broks Interface from pyro_daemon"
             self.pyro_daemon.unregister(self.ibroks.pyro_obj)
         #Create and connect it

commit 4664cedd7a8c551b7c33009ef718e7148d19faab

Fixed three sytax errors introduced in 08f30ec21347378e3d72b1ca45919504f767db63.

shinken/macroresolver.py
@@ -187,7 +187,7 @@ class MacroResolver(Borg):
             #Now we get values from elements
             for macro in macros:
                 #If type ARGN, look at ARGN cutting
-                if macros[macro]['type'] == 'ARGN' and argsis not None:
+                if macros[macro]['type'] == 'ARGN' and args is not None:
                     macros[macro]['val'] = self.resolve_argn(macro, args)
                     macros[macro]['type'] = 'resolved'
                 #If class, get value from properties

shinken/objects/timeperiod.py
@@ -212,7 +212,7 @@ class Timeperiod(Item):
 
             #Min but not the None valus...
             try:
-                local_min = min([d for d in dr_mins if dis notNone])
+                local_min = min([d for d in dr_mins if d is not None])
             except ValueError: #dr_mins if full of None, not good
                 local_min = None
 

shinken/schedulerlink.py
@@ -94,7 +94,7 @@ class SchedulerLink(SatelliteLink):
 
 
     def give_satellite_cfg(self):
-        return {'port' : self.port, 'address' : self.address, 'name' : self.scheduler_name, 'instance_id' : self.id, 'active' : self.confis notNone}
+        return {'port' : self.port, 'address' : self.address, 'name' : self.scheduler_name, 'instance_id' : self.id, 'active' : self.conf is not None}
 
 
     #Some parameters can give as 'overriden parameters' like use_timezone