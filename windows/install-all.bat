1c525dd4f0dd13ae15bb180e5b9d96f19e20a8e7
Gabes Jean <naparuba@users.sourceforge.net>
2011-03-10 16:19:29 +0100

    Add : receiver script for windows installation.

--- windows/install-all.bat
+++ windows/install-all.bat
@@ -3,6 +3,7 @@ move /Y c:\shinken\bin\shinken-reactionner c:\shinken\bin\shinken-reactionner.py
 move /Y c:\shinken\bin\shinken-scheduler c:\shinken\bin\shinken-scheduler.py
 move /Y c:\shinken\bin\shinken-arbiter c:\shinken\bin\shinken-arbiter.py
 move /Y c:\shinken\bin\shinken-broker c:\shinken\bin\shinken-broker.py
+move /Y c:\shinken\bin\shinken-receiver c:\shinken\bin\shinken-receiver.py
 
 
 c:\shinken\windows\instsrv.exe "Shinken-Arbiter" "c:\shinken\windows\srvany.exe"
@@ -14,3 +15,5 @@ c:\shinken\windows\instsrv.exe "Shinken-Poller" "c:\shinken\windows\srvany.exe"
 c:\shinken\windows\instsrv.exe "Shinken-Reactionner" "c:\shinken\windows\srvany.exe"
 
 c:\shinken\windows\instsrv.exe "Shinken-Broker" "c:\shinken\windows\srvany.exe"
+
+c:\shinken\windows\instsrv.exe "Shinken-Receiver" "c:\shinken\windows\srvany.exe"
--- windows/uninstall-all.bat
+++ windows/uninstall-all.bat
@@ -7,3 +7,5 @@ c:\shinken\windows\instsrv.exe "Shinken-Poller" remove
 c:\shinken\windows\instsrv.exe "Shinken-Reactionner" remove
 
 c:\shinken\windows\instsrv.exe "Shinken-Broker" remove
+
+c:\shinken\windows\instsrv.exe "Shinken-Receiver" remove
