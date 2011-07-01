842ec756294a74fb579f94d652e298b3a79d2aca
naparuba <naparuba@gmail.com>
2011-07-01 11:03:31 +0200

    Add: bench test for LS (disabled by default).

--- test/test_livestatus.py
+++ test/test_livestatus.py
@@ -2680,6 +2680,455 @@ OutputFormat: csv"""
 
 
 
+    # We look for the perf of the unhandled srv
+    # page view of Thruk. We only enable it when we need
+    # it's not a true test.
+    def test_thruk_unhandled_srv_page_perf(self):
+        # COMMENT THIS LINE to enable the bench and call
+        # python test_livestatus.py TestConfigBig.test_thruk_unhandled_srv_page_perf
+        return
+        import cProfile
+        cProfile.runctx('''self.do_test_thruk_unhandled_srv_page_perf()''', globals(), locals(),'/tmp/livestatus_thruk_perf.profile')
+        
+
+
+    def do_test_thruk_unhandled_srv_page_perf(self):
+        self.print_header()
+
+        objlist = []
+        # We put 10% of elemetnsi n bad states
+        i = 0
+        for host in self.sched.hosts:
+            i += 1
+            if i % 10 == 0:
+                objlist.append([host, 1, 'DOWN'])
+            else:
+                objlist.append([host, 0, 'UP'])
+        for service in self.sched.services:
+            i += 1
+            if i % 10 == 0:
+                objlist.append([service, 2, 'CRITICAL'])
+            else:
+                objlist.append([service, 0, 'OK'])
+        self.scheduler_loop(2, objlist)
+        self.update_broker()
+
+        # We will look for the overall page loading time
+        total_page = 0.0
+        
+        # First Query
+        query_start = time.time()
+        request = """
+GET status
+Columns: accept_passive_host_checks accept_passive_service_checks check_external_commands check_host_freshness check_service_freshness enable_event_handlers enable_flap_detection enable_notifications execute_host_checks execute_service_checks last_command_check last_log_rotation livestatus_version nagios_pid obsess_over_hosts obsess_over_services process_performance_data program_start program_version interval_length
+OutputFormat: json
+ResponseHeader: fixed16
+"""
+        print "Query 1 launched (Get overall status)"
+        response, keepalive = self.livestatus_broker.livestatus.handle_request(request)
+        #print response
+        load_time = time.time() - query_start
+        total_page += load_time
+        print "Response time 1 : %.3f" % load_time
+
+        #Second Query
+        query_start = time.time()
+        request = """
+GET hosts
+Stats: name !=
+StatsAnd: 1
+Stats: check_type = 0
+StatsAnd: 1
+Stats: check_type = 1
+StatsAnd: 1
+Stats: has_been_checked = 0
+StatsAnd: 1
+Stats: has_been_checked = 0
+Stats: active_checks_enabled = 0
+StatsAnd: 2
+Stats: has_been_checked = 0
+Stats: scheduled_downtime_depth > 0
+StatsAnd: 2
+Stats: has_been_checked = 1
+Stats: state = 0
+StatsAnd: 2
+Stats: check_type = 0
+Stats: has_been_checked = 1
+Stats: state = 0
+Stats: active_checks_enabled = 0
+StatsAnd: 4
+Stats: check_type = 1
+Stats: has_been_checked = 1
+Stats: state = 0
+Stats: active_checks_enabled = 0
+StatsAnd: 4
+Stats: has_been_checked = 1
+Stats: state = 0
+Stats: scheduled_downtime_depth > 0
+StatsAnd: 3
+Stats: has_been_checked = 1
+Stats: state = 1
+StatsAnd: 2
+Stats: has_been_checked = 1
+Stats: state = 1
+Stats: acknowledged = 1
+StatsAnd: 3
+Stats: has_been_checked = 1
+Stats: state = 1
+Stats: scheduled_downtime_depth > 0
+StatsAnd: 3
+Stats: check_type = 0
+Stats: has_been_checked = 1
+Stats: state = 1
+Stats: active_checks_enabled = 0
+StatsAnd: 4
+Stats: check_type = 1
+Stats: has_been_checked = 1
+Stats: state = 1
+Stats: active_checks_enabled = 0
+StatsAnd: 4
+Stats: has_been_checked = 1
+Stats: state = 1
+Stats: active_checks_enabled = 1
+Stats: acknowledged = 0
+Stats: scheduled_downtime_depth = 0
+StatsAnd: 5
+Stats: has_been_checked = 1
+Stats: state = 2
+StatsAnd: 2
+Stats: has_been_checked = 1
+Stats: state = 2
+Stats: acknowledged = 1
+StatsAnd: 3
+Stats: has_been_checked = 1
+Stats: state = 2
+Stats: scheduled_downtime_depth > 0
+StatsAnd: 3
+Stats: check_type = 0
+Stats: has_been_checked = 1
+Stats: state = 2
+Stats: active_checks_enabled = 0
+StatsAnd: 4
+Stats: check_type = 1
+Stats: has_been_checked = 1
+Stats: state = 2
+Stats: active_checks_enabled = 0
+StatsAnd: 4
+Stats: has_been_checked = 1
+Stats: state = 2
+Stats: active_checks_enabled = 1
+Stats: acknowledged = 0
+Stats: scheduled_downtime_depth = 0
+StatsAnd: 5
+Stats: is_flapping = 1
+StatsAnd: 1
+Stats: flap_detection_enabled = 0
+StatsAnd: 1
+Stats: notifications_enabled = 0
+StatsAnd: 1
+Stats: event_handler_enabled = 0
+StatsAnd: 1
+Stats: check_type = 0
+Stats: active_checks_enabled = 0
+StatsAnd: 2
+Stats: check_type = 1
+Stats: active_checks_enabled = 0
+StatsAnd: 2
+Stats: accept_passive_checks = 0
+StatsAnd: 1
+Stats: state = 1
+Stats: childs !=
+StatsAnd: 2
+OutputFormat: json
+ResponseHeader: fixed16
+"""
+        print "Query 2 launched (Get hosts stistics)"
+        response, keepalive = self.livestatus_broker.livestatus.handle_request(request)
+        #print response
+        load_time = time.time() - query_start
+        total_page += load_time
+        print "Response time 2 : %.3f" % load_time
+
+
+        # Now Query 3 (service stats)
+        query_start = time.time()
+        request = """
+GET services
+Stats: description !=
+StatsAnd: 1
+Stats: check_type = 0
+StatsAnd: 1
+Stats: check_type = 1
+StatsAnd: 1
+Stats: has_been_checked = 0
+StatsAnd: 1
+Stats: has_been_checked = 0
+Stats: active_checks_enabled = 0
+StatsAnd: 2
+Stats: has_been_checked = 0
+Stats: scheduled_downtime_depth > 0
+StatsAnd: 2
+Stats: has_been_checked = 1
+Stats: state = 0
+StatsAnd: 2
+Stats: has_been_checked = 1
+Stats: state = 0
+Stats: scheduled_downtime_depth > 0
+StatsAnd: 3
+Stats: check_type = 0
+Stats: has_been_checked = 1
+Stats: state = 0
+Stats: active_checks_enabled = 0
+StatsAnd: 4
+Stats: check_type = 1
+Stats: has_been_checked = 1
+Stats: state = 0
+Stats: active_checks_enabled = 0
+StatsAnd: 4
+Stats: has_been_checked = 1
+Stats: state = 1
+StatsAnd: 2
+Stats: has_been_checked = 1
+Stats: state = 1
+Stats: scheduled_downtime_depth > 0
+StatsAnd: 3
+Stats: check_type = 0
+Stats: has_been_checked = 1
+Stats: state = 1
+Stats: active_checks_enabled = 0
+StatsAnd: 4
+Stats: check_type = 1
+Stats: has_been_checked = 1
+Stats: state = 1
+Stats: active_checks_enabled = 0
+StatsAnd: 4
+Stats: has_been_checked = 1
+Stats: state = 1
+Stats: acknowledged = 1
+StatsAnd: 3
+Stats: has_been_checked = 1
+Stats: state = 1
+Stats: host_state != 0
+StatsAnd: 3
+Stats: has_been_checked = 1
+Stats: state = 1
+Stats: host_state = 0
+Stats: active_checks_enabled = 1
+Stats: acknowledged = 0
+Stats: scheduled_downtime_depth = 0
+StatsAnd: 6
+Stats: has_been_checked = 1
+Stats: state = 2
+StatsAnd: 2
+Stats: has_been_checked = 1
+Stats: state = 2
+Stats: scheduled_downtime_depth > 0
+StatsAnd: 3
+Stats: check_type = 0
+Stats: has_been_checked = 1
+Stats: state = 2
+Stats: active_checks_enabled = 0
+StatsAnd: 4
+Stats: check_type = 1
+Stats: has_been_checked = 1
+Stats: state = 2
+Stats: active_checks_enabled = 0
+StatsAnd: 4
+Stats: has_been_checked = 1
+Stats: state = 2
+Stats: acknowledged = 1
+StatsAnd: 3
+Stats: has_been_checked = 1
+Stats: state = 2
+Stats: host_state != 0
+StatsAnd: 3
+Stats: has_been_checked = 1
+Stats: state = 2
+Stats: host_state = 0
+Stats: active_checks_enabled = 1
+Stats: acknowledged = 0
+Stats: scheduled_downtime_depth = 0
+StatsAnd: 6
+Stats: has_been_checked = 1
+Stats: state = 3
+StatsAnd: 2
+Stats: has_been_checked = 1
+Stats: state = 3
+Stats: scheduled_downtime_depth > 0
+StatsAnd: 3
+Stats: check_type = 0
+Stats: has_been_checked = 1
+Stats: state = 3
+Stats: active_checks_enabled = 0
+StatsAnd: 4
+Stats: check_type = 1
+Stats: has_been_checked = 1
+Stats: state = 3
+Stats: active_checks_enabled = 0
+StatsAnd: 4
+Stats: has_been_checked = 1
+Stats: state = 3
+Stats: acknowledged = 1
+StatsAnd: 3
+Stats: has_been_checked = 1
+Stats: state = 3
+Stats: host_state != 0
+StatsAnd: 3
+Stats: has_been_checked = 1
+Stats: state = 3
+Stats: host_state = 0
+Stats: active_checks_enabled = 1
+Stats: acknowledged = 0
+Stats: scheduled_downtime_depth = 0
+StatsAnd: 6
+Stats: is_flapping = 1
+StatsAnd: 1
+Stats: flap_detection_enabled = 0
+StatsAnd: 1
+Stats: notifications_enabled = 0
+StatsAnd: 1
+Stats: event_handler_enabled = 0
+StatsAnd: 1
+Stats: check_type = 0
+Stats: active_checks_enabled = 0
+StatsAnd: 2
+Stats: check_type = 1
+Stats: active_checks_enabled = 0
+StatsAnd: 2
+Stats: accept_passive_checks = 0
+StatsAnd: 1
+OutputFormat: json
+ResponseHeader: fixed16
+"""
+        print "Query 3 launched (Get services statistics)"
+        response, keepalive = self.livestatus_broker.livestatus.handle_request(request)
+        #print response
+        load_time = time.time() - query_start
+        total_page += load_time
+        print "Response time 3 : %.3f" % load_time
+
+
+        # 4th Query
+        query_start = time.time()
+        request = """
+GET comments
+Columns: author comment entry_time entry_type expires expire_time host_name id persistent service_description source type
+Filter: service_description !=
+Filter: service_description =
+Or: 2
+OutputFormat: json
+ResponseHeader: fixed16
+"""
+        print "Query 4 launched (Get comments)"
+        response, keepalive = self.livestatus_broker.livestatus.handle_request(request)
+        #print response
+        load_time = time.time() - query_start
+        total_page += load_time
+        print "Response time 4 : %.3f" % load_time
+
+
+
+        # 5th Query
+        query_start = time.time()
+        request = """
+GET downtimes
+Columns: author comment end_time entry_time fixed host_name id start_time service_description triggered_by
+Filter: service_description !=
+Filter: service_description =
+Or: 2
+OutputFormat: json
+ResponseHeader: fixed16
+"""
+        print "Query 5 launched (Get downtimes)"
+        response, keepalive = self.livestatus_broker.livestatus.handle_request(request)
+        #print response
+        load_time = time.time() - query_start
+        total_page += load_time
+        print "Response time 5 : %.3f" % load_time
+
+
+
+        # 6th Query
+        query_start = time.time()
+        request = """
+REQUEST
+GET services
+Filter: host_has_been_checked = 0
+Filter: host_state = 0
+Filter: host_has_been_checked = 1
+And: 2
+Or: 2
+Filter: state = 1
+Filter: has_been_checked = 1
+And: 2
+Filter: state = 3
+Filter: has_been_checked = 1
+And: 2
+Filter: state = 2
+Filter: has_been_checked = 1
+And: 2
+Or: 3
+Filter: scheduled_downtime_depth = 0
+Filter: acknowledged = 0
+Filter: checks_enabled = 1
+And: 3
+And: 3
+Stats: description !=
+OutputFormat: json
+ResponseHeader: fixed16
+"""
+        print "Query 6 launched (Get bad services)"
+        response, keepalive = self.livestatus_broker.livestatus.handle_request(request)
+        #print response
+        load_time = time.time() - query_start
+        total_page += load_time
+        print "Response time 6 : %.3f" % load_time
+
+
+        # 7th Query
+        query_start = time.time()
+        request = """
+GET services
+Columns: accept_passive_checks acknowledged action_url action_url_expanded active_checks_enabled check_command check_interval check_options check_period check_type checks_enabled comments current_attempt current_notification_number description event_handler event_handler_enabled custom_variable_names custom_variable_values execution_time first_notification_delay flap_detection_enabled groups has_been_checked high_flap_threshold host_acknowledged host_action_url_expanded host_active_checks_enabled host_address host_alias host_checks_enabled host_comments host_groups host_has_been_checked host_icon_image_expanded host_icon_image_alt host_is_executing host_is_flapping host_name host_notes_url_expanded host_notifications_enabled host_scheduled_downtime_depth host_state icon_image icon_image_alt icon_image_expanded is_executing is_flapping last_check last_notification last_state_change latency long_plugin_output low_flap_threshold max_check_attempts next_check notes notes_expanded notes_url notes_url_expanded notification_interval notification_period notifications_enabled obsess_over_service percent_state_change perf_data plugin_output process_performance_data retry_interval scheduled_downtime_depth state state_type is_impact source_problems impacts criticity is_problem got_business_rule parent_dependencies
+Filter: host_has_been_checked = 0
+Filter: host_state = 0
+Filter: host_has_been_checked = 1
+And: 2
+Or: 2
+Filter: state = 1
+Filter: has_been_checked = 1
+And: 2
+Filter: state = 3
+Filter: has_been_checked = 1
+And: 2
+Filter: state = 2
+Filter: has_been_checked = 1
+And: 2
+Or: 3
+Filter: scheduled_downtime_depth = 0
+Filter: acknowledged = 0
+Filter: checks_enabled = 1
+And: 3
+And: 3
+Limit: 150
+OutputFormat: json
+ResponseHeader: fixed16
+"""
+        print "Query 7 launched (Get bad service data)"
+        response, keepalive = self.livestatus_broker.livestatus.handle_request(request)
+        #print response
+        load_time = time.time() - query_start
+        total_page += load_time
+        print "Response time 7 : %.3f" % load_time
+
+        print ""
+        print "Overall Queries time : %.3f" % total_page
+
+        
+
+
+        
+
 class TestConfigComplex(TestConfig):
     def setUp(self):
         self.setup_with_file('etc/nagios_problem_impact.cfg')
