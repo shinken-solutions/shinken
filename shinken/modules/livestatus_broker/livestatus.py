64b76fe5a1b2a51be62d69062495d0d35ce8ed35
naparuba <naparuba@gmail.com>
2011-07-01 14:50:02 +0200

    Enh : cut the livestatus.py file into anoter file. This time it should be ok.

--- shinken/modules/livestatus_broker/livestatus.py
+++ shinken/modules/livestatus_broker/livestatus.py
@@ -31,9 +31,7 @@ from hooker import Hooker
 from mapping import out_map
 from livestatus_counters import LiveStatusCounters
 from log_line import Logline
-from livestatus_query import LiveStatusQuery
-from livestatus_wait_query import LiveStatusWaitQuery
-from livestatus_command_query import LiveStatusCommandQuery
+from livestatus_request import LiveStatusRequest
 
 
 
@@ -248,82 +246,6 @@ class LiveStatus(object, Hooker):
 
     def count_event(self, counter):
         self.counters.increment(counter)
-
-
-
-
-
-class LiveStatusRequest(LiveStatus):    
-   
-    """A class describing a livestatus request."""
-    
-    def __init__(self, data, configs, hosts, services, contacts, hostgroups, servicegroups, contactgroups, timeperiods, commands, schedulers, pollers, reactionners, brokers, dbconn, pnp_path, return_queue, counters):
-        self.data = data
-        # Runtime data form the global LiveStatus object
-        self.configs = configs
-        self.hosts = hosts
-        self.services = services
-        self.contacts = contacts
-        self.hostgroups = hostgroups
-        self.servicegroups = servicegroups
-        self.contactgroups = contactgroups
-        self.timeperiods = timeperiods
-        self.commands = commands
-        self.schedulers = schedulers
-        self.pollers = pollers
-        self.reactionners = reactionners
-        self.brokers = brokers
-        self.dbconn = dbconn
-        self.pnp_path = pnp_path
-        self.return_queue = return_queue
-        self.counters = counters
-
-        self.queries = []
-        # Set a timestamp for this specific request
-        self.tic = time.time()
-
-
-    def parse_input(self, data):
-        """Parse the lines of a livestatus request.
-        
-        This function looks for keywords in input lines and
-        sets the attributes of the request object
-        
-        """
-        external_cmds = []
-        query_cmds = []
-        wait_cmds = []
-        for line in data.splitlines():
-            line = line.strip()
-            # Tools like NagVis send KEYWORK:option, and we prefer to have
-            # a space folowing the :
-            if ':' in line and not ' ' in line:
-                line = line.replace(':', ': ')
-            keyword = line.split(' ')[0].rstrip(':')
-            if len(line) == 0:
-                pass
-            elif keyword in ('GET'):
-                query_cmds.append(line)
-                wait_cmds.append(line)
-            elif keyword in ('WaitObject', 'WaitCondition', 'WaitConditionOr', 'WaitConditionAnd', 'WaitTrigger', 'WaitTimeout'):
-                wait_cmds.append(line)
-            elif keyword in ('COMMAND'):
-                external_cmds.append(line)
-            else:
-                query_cmds.append(line)
-        if len(external_cmds) > 0:
-            for external_cmd in external_cmds:
-                query = LiveStatusCommandQuery(self.configs, self.hosts, self.services, self.contacts, self.hostgroups, self.servicegroups, self.contactgroups, self.timeperiods, self.commands, self.schedulers, self.pollers, self.reactionners, self.brokers, self.dbconn, self.pnp_path, self.return_queue, self.counters)
-                query.parse_input(external_cmd)
-                self.queries.append(query)
-        if len(wait_cmds) > 1:
-            query = LiveStatusWaitQuery(self.configs, self.hosts, self.services, self.contacts, self.hostgroups, self.servicegroups, self.contactgroups, self.timeperiods, self.commands, self.schedulers, self.pollers, self.reactionners, self.brokers, self.dbconn, self.pnp_path, self.return_queue, self.counters)
-            query.parse_input('\n'.join(wait_cmds))
-            self.queries.append(query)
-        if len(query_cmds) > 0:
-            query = LiveStatusQuery(self.configs, self.hosts, self.services, self.contacts, self.hostgroups, self.servicegroups, self.contactgroups, self.timeperiods, self.commands, self.schedulers, self.pollers, self.reactionners, self.brokers, self.dbconn, self.pnp_path, self.return_queue, self.counters)
-            query.parse_input('\n'.join(query_cmds))
-            self.queries.append(query)
 
 
 
