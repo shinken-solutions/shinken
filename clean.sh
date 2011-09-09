9e42d7e83080e20784bcbfe560ce4ea2870096ab
naparuba <naparuba@gmail.com>
2011-09-09 12:55:25 +0200

    Add : more clean with the clean file.

--- clean.sh
+++ clean.sh
@@ -11,7 +11,7 @@ sudo rm -fr /etc/init.d/shinken*
 sudo rm -fr /var/lib/shinken
 sudo rm -fr test/var/*.pid
 sudo rm -fr var/*.debug
-sudo rm -fr var/*.log
+sudo rm -fr var/*.log*
 sudo rm -fr var/*.pid
 sudo rm -fr var/service-perfdata
 sudo rm -fr var/*.dat
