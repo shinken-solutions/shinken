99bfb1375078b60f44c648b6ec644ebf7de27159
naparuba <naparuba@gmail.com>
2011-09-09 11:14:28 +0200

    Fix : nowthe box for 3dimpacst is better.

--- shinken/webui/plugins/impacts/htdocs/js/multi.js
+++ shinken/webui/plugins/impacts/htdocs/js/multi.js
@@ -5,8 +5,8 @@
 window.addEvent('domready', function(){ 
 	var box = new MultiBox('mb', { 
 		/*overlay: over*/
-		movieWidth : 800,
-		movieHeight : 800
+		movieWidth : 1200,
+		movieHeight : 600
 		
 	    }); 
     });
