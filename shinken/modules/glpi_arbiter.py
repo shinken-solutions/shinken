35157fe9b11c9e241275977309f8dd2c9c8c6f05
David Durieux <d.durieux@siprossii.com>
2011-09-16 15:48:15 +0200

    Update indent

--- shinken/modules/glpi_arbiter.py
+++ shinken/modules/glpi_arbiter.py
@@ -157,20 +157,20 @@ class Glpi_arbiter(BaseModule):
             h = {'host_name' : service_info['host_name'],
                  'service_description' : service_info['service_description']
                 }
-                if "service_info['use']" in locals():
-                    h = {'use' : service_info['use']};
-                if "service_info['check_command']" in locals():
-                    h = {'check_command' : service_info['check_command']};
-                if "service_info['check_interval']" in locals():
-                    h = {'check_interval' : service_info['check_interval']};
-                if "service_info['check_interval']" in locals():
-                    h = {'retry_interval' : service_info['retry_interval']};
-                if "service_info['max_check_attempts']" in locals():
-                    h = {'max_check_attempts' : service_info['max_check_attempts']};
-                if "service_info['check_period']" in locals():
-                    h = {'check_period' : service_info['check_period']};
-                if "service_info['contacts']" in locals():
-                    h = {'contacts' : service_info['contacts']};                 
+            if "service_info['use']" in locals():
+                h = {'use' : service_info['use']};
+            if "service_info['check_command']" in locals():
+                h = {'check_command' : service_info['check_command']};
+            if "service_info['check_interval']" in locals():
+                h = {'check_interval' : service_info['check_interval']};
+            if "service_info['check_interval']" in locals():
+                h = {'retry_interval' : service_info['retry_interval']};
+            if "service_info['max_check_attempts']" in locals():
+                h = {'max_check_attempts' : service_info['max_check_attempts']};
+            if "service_info['check_period']" in locals():
+                h = {'check_period' : service_info['check_period']};
+            if "service_info['contacts']" in locals():
+                h = {'contacts' : service_info['contacts']};                 
             r['services'].append(h)
 
         #print "Returning to Arbiter the hosts:", r
