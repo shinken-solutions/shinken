b2ff0c2d56eef0af51ee8622d57ef649ac4ef39f
Gerhard Lausser <gerhard.lausser@consol.de>
2011-10-04 01:23:03 +0200

    *Allow creation of a Notification object without a ref

--- shinken/notification.py
+++ shinken/notification.py
@@ -114,11 +114,14 @@ class Notification(Action):
         self.ref = ref
 
         # Set host_name and description from the ref
-        self.host_name = self.ref.host_name        
-        if self.ref.__class__.my_type == 'service':
+        try:
+            self.host_name = self.ref.host_name        
+        except:
+            self.host_name = host_name        
+        try:
             self.service_description = self.ref.service_description
-        else:
-            self.service_description = ''
+        except:
+            self.service_description = service_description
 
         self.env = env
         self.module_type = module_type
