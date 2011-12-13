2ee9a644bc3465dc63b372f32379b9854c5bbb62
Naparuba <naparuba@gmail.com>
2011-12-13 15:52:27 +0100

    Add : left/right effect on the mobile impacts page.

---
+++ shinken/webui/plugins/mobile/htdocs/js/mobile_impacts.js
@@ -0,0 +1,78 @@
+/*Copyright (C) 2009-2011 :
+     Gabes Jean, naparuba@gmail.com
+     Gerhard Lausser, Gerhard.Lausser@consol.de
+     Gregory Starck, g.starck@gmail.com
+     Hartmut Goebel, h.goebel@goebel-consult.de
+     Andreas Karfusehr, andreas@karfusehr.de
+ 
+ This file is part of Shinken.
+ 
+ Shinken is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Shinken is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+ 
+ You should have received a copy of the GNU Affero General Public License
+ along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+var offset = 100;
+
+var nb_impacts = 0;
+var current_idx = 0;
+
+function focus_on(idx){
+    
+
+    var first_right = $('impact-1');
+    if(first_right != null){
+	first_right.style.opacity = '0.7';
+    }
+
+    // And make others nearly disapears
+    for(var j=0;j<nb_impacts;j++){
+	var impact = $('impact-'+j);
+	if(j == idx){
+	    impact.style.opacity = '1';
+	    impact.style.left = ''+offset+'px';
+	}else if(j == idx-1 || j == idx+1){
+	    impact.style.opacity = '0.7';
+	    impact.style.left = ''+((j-idx)*250+offset)+'px';
+	}else{
+	    impact.style.opacity = '0.1';
+	    impact.style.left = ''+((j-idx)*250+offset)+'px';
+	}
+    }
+    
+}
+
+
+function go_left(){
+    if(current_idx > 0){
+	current_idx -= 1;
+	focus_on(current_idx);
+    }
+}
+
+function go_right(){
+    if(current_idx < nb_impacts-1){
+	current_idx += 1;
+	focus_on(current_idx);
+    }
+}
+
+
+window.addEvent('domready', function(){
+
+    // First we make opacity low for distant
+    var all_impacts = $$('.impact');
+    nb_impacts = all_impacts.length;
+
+    focus_on(0);
+});
--- shinken/webui/plugins/mobile/views/mobile_impacts.tpl
+++ shinken/webui/plugins/mobile/views/mobile_impacts.tpl
@@ -1,5 +1,8 @@
 
-%rebase layout globals(), title="Tactical view", js=['mobile/js/mobile_main.js'], css=['mobile/css/main.css', 'mobile/css/impacts.css']
+%helper = app.helper
+%datamgr = app.datamgr
+
+%rebase layout globals(), title="Tactical view", js=['mobile/js/mobile_main.js', 'mobile/js/mobile_impacts.js'], css=['mobile/css/main.css', 'mobile/css/impacts.css']
 
 <div id="all">
 <div> <h1> Shinken business apps</h1> </div>
@@ -8,13 +11,32 @@
 
 
 
-<img src="/static/images/state_critical.png" >
-<a href="#" onclick="slide_and_go('/mobile/impacts');"><img src="/static/images/next.png"/></a>
+<a href="#" onclick="go_left();"><img src="/static/images/previous.png"/></a>
+<a href="#" onclick="go_right();"><img src="/static/images/next.png"/></a>
+<br/>
 %i = 0
+
 <div class="impacts">
-  %for imp in impacts:
-    <div class="impact" style="left:{{i*250}}px">
-      <p class="{{imp.state.lower()}}">{{imp.get_full_name()}} is {{imp.state}}</p>
+  %for impact in impacts:
+    <div class="impact" id="impact-{{i}}" style="left:{{i*250}}px;">
+      <p class="{{impact.state.lower()}}">{{impact.get_full_name()}} is {{impact.state}}</p>
+
+      %for j in range(2, impact.business_impact):
+      <div class="criticity-inpb-icon-{{j-1}}">
+	<img src="/static/images/star.png">
+      </div>
+      %end
+      <h2 class="state_{{impact.state.lower()}}"><img style="width : 64px; height:64px" src="{{helper.get_icon_state(impact)}}" />{{impact.state}}: {{impact.get_full_name()}}</h2>
+		
+    %if len(impact.parent_dependencies) > 0:
+      <a id="togglelink-{{impact.get_dbg_name()}}" href="javascript:toggleBusinessElt('{{impact.get_dbg_name()}}')"> {{!helper.get_button('Show dependency tree', img='/static/images/expand.png')}}</a>
+      <div class="clear"></div>
+      {{!helper.print_business_rules(datamgr.get_business_parents(impact))}}
+    %end  
+
+
+
+
     </div>
     %i += 1
   %end
