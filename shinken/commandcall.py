shinken-solutions/shinken

commit cf505337f3dfdf59c465bdfe2a7045d19c58871e

Fix : (reported by: nadiw) protect the escalmation point if anti-slash before in commands args.

shinken/commandcall.py
@@ -62,9 +62,11 @@ class CommandCall(DummyCommandCall):
         self.id = self.__class__.id
         self.__class__.id += 1
         self.call = call
-        tab = call.split('!')
-        self.command = tab[0]
-        self.args = tab[1:]
+        # Now split by ! and get command and args
+        self.get_command_and_args()
+        #tab = call.split('!')
+        #self.command = tab[0]
+        #self.args = tab[1:]
         self.command = commands.find_by_name(self.command.strip())
         if self.command is not None:
             self.valid = True
@@ -83,6 +85,17 @@ class CommandCall(DummyCommandCall):
             if self.valid and reactionner_tag is 'None':
                 self.reactionner_tag = self.command.reactionner_tag #from command if not set
 
+    # We want to get the command and the args with ! splitting.
+    # but don't forget to protect against the \! to do not split them
+    def get_command_and_args(self):
+        # first protect
+        p_call = self.call.replace('\!', '___PROTECT_ESCLAMATION___')
+        tab = p_call.split('!')
+        self.command = tab[0]
+        # Reverse the protection
+        self.args = [s.replace('___PROTECT_ESCLAMATION___', '!') for s in tab[1:]]
+        
+        
 
 
     def is_valid(self):

test/etc/nagios_protect_esclamation_point.cfg
@@ -1,0 +1,119 @@
+accept_passive_host_checks=1
+accept_passive_service_checks=1
+additional_freshness_latency=15
+admin_email=shinken@localhost
+admin_pager=shinken@localhost
+auto_reschedule_checks=0
+auto_rescheduling_interval=30
+auto_rescheduling_window=180
+cached_host_check_horizon=15
+cached_service_check_horizon=15
+cfg_file=protect_esclamation_point/hosts.cfg
+cfg_file=protect_esclamation_point/services.cfg
+cfg_file=protect_esclamation_point/contacts.cfg
+cfg_file=protect_esclamation_point/commands.cfg
+cfg_file=protect_esclamation_point/timeperiods.cfg
+cfg_file=protect_esclamation_point/hostgroups.cfg
+cfg_file=protect_esclamation_point/servicegroups.cfg
+cfg_file=protect_esclamation_point/shinken-specific.cfg
+check_external_commands=1
+check_for_orphaned_hosts=1
+check_for_orphaned_services=1
+check_host_freshness=0
+check_result_path=var/spool/checkresults
+check_result_reaper_frequency=10
+check_service_freshness=1
+command_check_interval=-1
+command_file=var/shinken.cmd
+daemon_dumps_core=0
+date_format=iso8601
+debug_file=var/shinken.debug
+debug_level=112
+debug_verbosity=1
+enable_embedded_perl=0
+enable_environment_macros=1
+enable_event_handlers=1
+enable_flap_detection=0
+enable_notifications=1
+enable_predictive_host_dependency_checks=1
+enable_predictive_service_dependency_checks=1
+event_broker_options=-1
+event_handler_timeout=30
+execute_host_checks=1
+execute_service_checks=1
+external_command_buffer_slots=4096
+high_host_flap_threshold=20
+high_service_flap_threshold=20
+host_check_timeout=30
+host_freshness_check_interval=60
+host_inter_check_delay_method=s
+illegal_macro_output_chars=`~\$&|'"<>
+illegal_object_name_chars=`~!\$%^&*|'"<>?,()=
+interval_length=60
+lock_file=var/shinken.pid
+log_archive_path=var/archives
+log_event_handlers=1
+log_external_commands=1
+log_file=var/nagios.log
+log_host_retries=1
+log_initial_states=0
+log_notifications=1
+log_passive_checks=1
+log_rotation_method=d
+log_service_retries=1
+low_host_flap_threshold=5
+low_service_flap_threshold=5
+max_check_result_file_age=3600
+max_check_result_reaper_time=30
+max_concurrent_checks=0
+max_debug_file_size=1000000
+max_host_check_spread=30
+max_service_check_spread=30
+nagios_group=shinken
+nagios_user=shinken
+notification_timeout=30
+object_cache_file=var/objects.cache
+obsess_over_hosts=0
+obsess_over_services=0
+ocsp_timeout=5
+#p1_file=/tmp/test_shinken/plugins/p1.pl
+p1_file=/usr/local/nagios/bin/p1.pl
+passive_host_checks_are_soft=0
+perfdata_timeout=5
+precached_object_file=var/objects.precache
+process_performance_data=1
+resource_file=resource.cfg
+retain_state_information=1
+retained_contact_host_attribute_mask=0
+retained_contact_service_attribute_mask=0
+retained_host_attribute_mask=0
+retained_process_host_attribute_mask=0
+retained_process_service_attribute_mask=0
+retained_service_attribute_mask=0
+retention_update_interval=60
+service_check_timeout=60
+service_freshness_check_interval=60
+service_inter_check_delay_method=s
+service_interleave_factor=s
+##shinken_group=shinken
+##shinken_user=shinken
+#nagios_group=nagios
+#nagios_user=nagios
+sleep_time=0.25
+soft_state_dependencies=0
+state_retention_file=var/retention.dat
+status_file=var/status.dat
+status_update_interval=5
+temp_file=tmp/shinken.tmp
+temp_path=var/tmp
+translate_passive_host_checks=0
+use_aggressive_host_checking=0
+use_embedded_perl_implicitly=0
+use_large_installation_tweaks=0
+use_regexp_matching=0
+use_retained_program_state=1
+use_retained_scheduling_info=1
+use_syslog=0
+use_true_regexp_matching=0
+enable_problem_impacts_states_change=1
+no_event_handlers_during_downtimes=1

test/etc/protect_esclamation_point/commands.cfg
@@ -1,0 +1,30 @@
+define command{
+    command_name    check-host-alive
+    command_line    $USER1$/test_hostcheck.pl --type=$ARG1$ --failchance=2% --previous-state=$HOSTSTATE$ --state-duration=$HOSTDURATIONSEC$ --hostname $HOSTNAME$
+}
+define command{
+    command_name    check-host-alive-parent
+    command_line    $USER1$/test_hostcheck.pl --type=$ARG1$ --failchance=2% --previous-state=$HOSTSTATE$ --state-duration=$HOSTDURATIONSEC$ --parent-state=$ARG2$ --hostname $HOSTNAME$
+}
+define command{
+    command_name    notify-host
+    #command_line    sleep 1 && /bin/true
+    command_line    $USER1$/notifier.pl --hostname $HOSTNAME$ --notificationtype $NOTIFICATIONTYPE$ --hoststate $HOSTSTATE$ --hostoutput $HOSTOUTPUT$ --longdatetime $LONGDATETIME$ --hostattempt $HOSTATTEMPT$ --hoststatetype $HOSTSTATETYPE$
+}
+define command{
+    command_name    notify-service
+    command_line    $USER1$/notifier.pl --hostname $HOSTNAME$ --servicedesc $SERVICEDESC$ --notificationtype $NOTIFICATIONTYPE$ --servicestate $SERVICESTATE$ --serviceoutput $SERVICEOUTPUT$ --longdatetime $LONGDATETIME$ --serviceattempt $SERVICEATTEMPT$ --servicestatetype $SERVICESTATETYPE$
+    #command_line    sleep 1 && /bin/true
+}
+define command{
+    command_name    check_service
+    command_line    $USER1$/test_servicecheck.pl --type=$ARG1$ --failchance=5% --previous-state=$SERVICESTATE$ --state-duration=$SERVICEDURATIONSEC$ --total-critical-on-host=$TOTALHOSTSERVICESCRITICAL$ --total-warning-on-host=$TOTALHOSTSERVICESWARNING$ --hostname $HOSTNAME$ --servicedesc $SERVICEDESC$ --custom $_SERVICECUSTNAME$
+}
+define command{
+  command_name eventhandler
+  command_line    $USER1$/test_eventhandler.pl $SERVICESTATE$ $SERVICESTATETYPE$ $SERVICEATTEMPT$
+}
+define command{
+  command_name    special_macro
+  command_line	  $USER1$/nothing $ARG1$
+}

test/etc/protect_esclamation_point/contacts.cfg
@@ -1,0 +1,18 @@
+define contactgroup{
+    contactgroup_name       test_contact
+    alias                   test_contacts_alias
+    members                 test_contact
+}
+
+define contact{
+    contact_name                    test_contact
+    alias                           test_contact_alias
+    service_notification_period     24x7
+    host_notification_period        24x7
+    service_notification_options    w,u,c,r,f
+    host_notification_options       d,u,r,f,s
+    service_notification_commands   notify-service
+    host_notification_commands      notify-host
+    email                           nobody@localhost
+    can_submit_commands             1
+}

test/etc/protect_esclamation_point/hostgroups.cfg
@@ -1,0 +1,62 @@
+
+define hostgroup {
+    hostgroup_name          router
+    alias                   All Router Hosts
+}
+
+define hostgroup {
+    hostgroup_name          hostgroup_01
+    alias                   hostgroup_alias_01
+}
+
+define hostgroup {
+    hostgroup_name          hostgroup_02
+    alias                   hostgroup_alias_02
+}
+
+define hostgroup {
+    hostgroup_name          hostgroup_03
+    alias                   hostgroup_alias_03
+}
+
+define hostgroup {
+    hostgroup_name          hostgroup_04
+    alias                   hostgroup_alias_04
+}
+
+define hostgroup {
+    hostgroup_name          hostgroup_05
+    alias                   hostgroup_alias_05
+}
+
+define hostgroup {
+    hostgroup_name          up
+    alias                   All Up Hosts
+}
+
+define hostgroup {
+    hostgroup_name          down
+    alias                   All Down Hosts
+}
+
+define hostgroup {
+    hostgroup_name          pending
+    alias                   All Pending Hosts
+}
+
+define hostgroup {
+    hostgroup_name          random
+    alias                   All Random Hosts
+}
+
+define hostgroup {
+    hostgroup_name          flap
+    alias                   All Flapping Hosts
+}
+
+define hostgroup {
+    hostgroup_name          allhosts
+    alias                   All Hosts
+    members                 test_router_0,test_host_0
+}
+

test/etc/protect_esclamation_point/hosts.cfg
@@ -1,0 +1,52 @@
+define host{
+  check_interval                 1
+  check_period                   24x7
+  contact_groups                 test_contact
+  event_handler_enabled          1
+  failure_prediction_enabled     1
+  flap_detection_enabled         1
+  max_check_attempts             3
+  name                           generic-host
+  notification_interval          1
+  notification_options           d,u,r,f,s
+  notification_period            24x7
+  notifications_enabled          1
+  process_perf_data              1
+  register                       0
+  retain_nonstatus_information   1
+  retain_status_information      1
+  retry_interval                 1
+  notes_url                      /nagios/wiki/doku.php/$HOSTNAME$
+  action_url                     /nagios/pnp/index.php?host=$HOSTNAME$
+}
+
+define host{
+  action_url                     http://search.cpan.org/dist/Monitoring-Generator-TestConfig/
+  address                        127.0.0.1
+  alias                          flap_0
+  check_command                  check-host-alive!flap
+  check_period                   24x7
+  host_name                      test_router_0
+  hostgroups                     router
+  icon_image                     ../../docs/images/switch.png?host=$HOSTNAME$
+  icon_image_alt                 icon alt string
+  notes                          just a notes string
+  notes_url                      http://search.cpan.org/dist/Monitoring-Generator-TestConfig/README
+  use                            generic-host
+}
+
+define host{
+  address                        127.0.0.1
+  alias                          up_0
+  check_command                  check-host-alive-parent!up!$HOSTSTATE:test_router_0$
+  event_handler                  eventhandler
+  check_period                   24x7
+  host_name                      test_host_0
+  hostgroups                     hostgroup_01,up
+  parents                        test_router_0
+  use                            generic-host
+  criticity			 5
+  _ostype			 gnulinux
+  _oslicense			 gpl
+}
+

test/etc/protect_esclamation_point/servicegroups.cfg
@@ -1,0 +1,61 @@
+
+define servicegroup {
+    servicegroup_name       servicegroup_01
+    alias                   servicegroup_alias_01
+}
+
+define servicegroup {
+    servicegroup_name       servicegroup_02
+    alias                   servicegroup_alias_02
+    members                 test_host_0,test_ok_0
+}
+
+define servicegroup {
+    servicegroup_name       servicegroup_03
+    alias                   servicegroup_alias_03
+}
+
+define servicegroup {
+    servicegroup_name       servicegroup_04
+    alias                   servicegroup_alias_04
+}
+
+define servicegroup {
+    servicegroup_name       servicegroup_05
+    alias                   servicegroup_alias_05
+}
+
+define servicegroup {
+    servicegroup_name       ok
+    alias                   All Ok Services
+}
+
+define servicegroup {
+    servicegroup_name       warning
+    alias                   All Warning Services
+}
+
+define servicegroup {
+    servicegroup_name       unknown
+    alias                   All Unknown Services
+}
+
+define servicegroup {
+    servicegroup_name       critical
+    alias                   All Critical Services
+}
+
+define servicegroup {
+    servicegroup_name       pending
+    alias                   All Pending Services
+}
+
+define servicegroup {
+    servicegroup_name       random
+    alias                   All Random Services
+}
+
+define servicegroup {
+    servicegroup_name       flap
+    alias                   All Flapping Services
+}

test/etc/protect_esclamation_point/services.cfg
@@ -1,0 +1,44 @@
+define service{
+  active_checks_enabled          1
+  check_freshness                0
+  check_interval                 1
+  check_period                   24x7
+  contact_groups                 test_contact
+  event_handler_enabled          1
+  failure_prediction_enabled     1
+  flap_detection_enabled         1
+  is_volatile                    0
+  max_check_attempts             2
+  name                           generic-service
+  notification_interval          1
+  notification_options           w,u,c,r,f,s
+  notification_period            24x7
+  notifications_enabled          1
+  obsess_over_service            1
+  parallelize_check              1
+  passive_checks_enabled         1
+  process_perf_data              1
+  register                       0
+  retain_nonstatus_information   1
+  retain_status_information      1
+  retry_interval                 1
+}
+
+define service{
+  active_checks_enabled          1
+  check_command                  check_service!blabla!ti\!ti!toto
+  check_interval                 1
+  host_name                      test_host_0
+  icon_image                     ../../docs/images/tip.gif?host=$HOSTNAME$&srv=$SERVICEDESC$
+  icon_image_alt                 icon alt string
+  notes                          just a notes string
+  retry_interval                 1
+  service_description            test_ok_0
+  servicegroups                  servicegroup_01,ok
+  use                            generic-service
+  event_handler                  eventhandler
+  notes_url                      /nagios/wiki/doku.php/$HOSTNAME$/$SERVICEDESC$
+  action_url                     /nagios/pnp/index.php?host=$HOSTNAME$&srv=$SERVICEDESC$
+  _custname			 custvalue
+}
+

commit 1bf2051c93c307aa040f4b3164c35b2c486e79fe

Fix a bug in commandcall which was introduced with cf505337f3

shinken/commandcall.py
@@ -72,7 +72,6 @@ class CommandCall(DummyCommandCall):
             self.valid = True
         else:
             self.valid = False
-            self.command = tab[0]
         if self.valid:
             # If the host/service do not give an override poller_tag, take
             # the one of the command