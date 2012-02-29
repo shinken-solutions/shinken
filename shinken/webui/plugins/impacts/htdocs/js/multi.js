81967b4e2c8d0da5d46b3e4da14aa713d440da18
Naparuba <naparuba@gmail.com>
2012-02-29 14:45:48 +0100

    Fix : impact view for jquery. I miss a lot Mootools Tween :p, animate is really under tween power.

--- shinken/webui/plugins/impacts/htdocs/js/multi.js
+++ shinken/webui/plugins/impacts/htdocs/js/multi.js
@@ -24,11 +24,12 @@
 
 /* We pin in the buttong MB a overlay box of 800*800, for
  the 3dimpact page*/
-window.addEvent('domready', function(){ 
+
+/*window.addEvent('domready', function(){ 
 	var box = new MultiBox('mb', { 
-		/*overlay: over*/
 		movieWidth : 1200,
 		movieHeight : 600
 		
 	    }); 
     });
+*/
