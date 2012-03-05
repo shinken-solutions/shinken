77c014e8166b75f35fd9062eef88d882e585b277
Thibault Cohen <thibault.cohen@savoirfairelinux.com>
2012-03-05 18:56:06 -0500

    * Fix Pep8 violations

--- shinken/basemodule.py
+++ shinken/basemodule.py
@@ -22,7 +22,9 @@
 # along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
 
 
-""" This python module contains the class BaseModule that shinken modules will subclass """
+""" This python module contains the class BaseModule
+that shinken modules will subclass
+"""
 
 
 import os
@@ -31,11 +33,9 @@ import time
 from multiprocessing import Queue, Process
 
 from shinken.log import logger
- 
 
-
-
-# TODO: use a class for defining the module "properties" instead of plain dict ??  Like:
+# TODO: use a class for defining the module "properties" instead of
+# plain dict ??  Like:
 '''
 class ModuleProperties(object):
     def __init__(self, type, phases, external=False)
@@ -48,43 +48,46 @@ class ModuleProperties(object):
 properties = ModuleProperties('the_module_type', the_module_phases, is_mod_ext)
 '''
 
-
-# The `properties´ dict defines what the module can do and if it's an external module or not. 
+# The `properties´ dict defines what the module can do and
+# if it's an external module or not.
 properties = {
     # name of the module type ; to distinguish between them:
     'type': None,
-    
+
     # is the module "external" (external means here a daemon module) ?
     'external': True,
-    
+
     # Possible configuration phases where the module is involved :
-    'phases' : [ 'configuration', 'late_configuration', 'running', 'retention' ], 
+    'phases': ['configuration', 'late_configuration', 'running', 'retention'],
     }
 
 
-""" TODO : Add some comment about this class for the doc"""
 class ModulePhases:
-# TODO: why not use simply integers instead of string to represent the different phases ??    
-    CONFIGURATION       = 1
-    LATE_CONFIGURATION  = 2
-    RUNNING             = 4 
-    RETENTION           = 8
-    
+    """TODO : Add some comment about this class for the doc"""
+    # TODO: why not use simply integers instead of string
+    # to represent the different phases ??
+    CONFIGURATION = 1
+    LATE_CONFIGURATION = 2
+    RUNNING = 4
+    RETENTION = 8
 
-""" This is the base class for the shinken modules.
-Modules can be used by the different shinken daemons/services for different tasks.
-Example of task that a shinken module can do:
- - load additional configuration objects.
- - recurrently save hosts/services status/perfdata informations in different format.
- - ...
- 
- """
+
 class BaseModule(object):
-
-
+    """This is the base class for the shinken modules.
+    Modules can be used by the different shinken daemons/services
+    for different tasks.
+    Example of task that a shinken module can do:
+     - load additional configuration objects.
+     - recurrently save hosts/services status/perfdata
+       informations in different format.
+     - ...
+     """
     def __init__(self, mod_conf):
-        # Instanciate a new module. There can be many instance of the same type.
-        #`mod_conf´ is module configuration object for this new module instance.
+        """Instanciate a new module.
+        There can be many instance of the same type.
+        `mod_conf´ is module configuration object
+        for this new module instance.
+        """
         self.myconf = mod_conf
         self.name = mod_conf.get_name()
         # We can have sub modules
@@ -92,28 +95,31 @@ class BaseModule(object):
         self.props = mod_conf.properties.copy()
         # TODO: choose between 'props' or 'properties'..
         self.interrupted = False
-        self.properties = self.props 
+        self.properties = self.props
         self.is_external = self.props.get('external', False)
         # though a module defined with no phase is quite useless .
-        self.phases = self.props.get('phases', []) 
+        self.phases = self.props.get('phases', [])
         self.phases.append(None)
         # the queue the module will receive data to manage
-        self.to_q = None 
+        self.to_q = None
         # the queue the module will put its result data
-        self.from_q = None 
+        self.from_q = None
         self.process = None
         self.init_try = 0
 
-    # Handle this module "post" init ; just before it'll be started. 
-    # Like just open necessaries file(s), database(s), or whatever the module will need.
     def init(self):
+        """Handle this module "post" init ; just before it'll be started.
+        Like just open necessaries file(s), database(s),
+        or whatever the module will need.
+        """
         pass
 
-    # The manager is None on android, but a true Manager() elsewhere
-    # Create the shared queues that will be used by shinken daemon process and this module process.
-    # But clear queues if they were already set before recreating new one.  
     def create_queues(self, manager=None):
-
+        """The manager is None on android, but a true Manager() elsewhere
+        Create the shared queues that will be used by shinken daemon
+        process and this module process.
+        But clear queues if they were already set before recreating new one.
+        """
         self.clear_queues(manager)
         # If no Manager() object, go with classic Queue()
         if not manager:
@@ -123,11 +129,11 @@ class BaseModule(object):
             self.from_q = manager.Queue()
             self.to_q = manager.Queue()
 
-
-    # Release the resources associated to the queues of this instance
     def clear_queues(self, manager):
+        """Release the resources associated to the queues of this instance"""
         for q in (self.to_q, self.from_q):
-            if q is None: continue
+            if q is None:
+                continue
             # If we got no manager, we direct call the clean
             if not manager:
                 q.close()
@@ -150,20 +156,21 @@ class BaseModule(object):
         # its process as object, so we remove it and we set it in a earlier
         # start
         try:
-          del self.properties['process']
+            del self.properties['process']
         except:
-          pass
+            pass
 
         p.start()
         # We save the process data AFTER the fork()
         self.process = p
-        self.properties['process'] = p  ## TODO: temporary
+        self.properties['process'] = p  # TODO: temporary
         logger.log("%s is now started ; pid=%d" % (self.name, p.pid))
 
-
-    # Sometime terminate() is not enough, we must "help"
-    # external modules to die...
     def __kill(self):
+        """Sometime terminate() is not enough, we must "help"
+        external modules to die...
+        """
+
         if os.name == 'nt':
             self.process.terminate()
         else:
@@ -174,10 +181,11 @@ class BaseModule(object):
             if self.process.is_alive():
                 os.kill(self.process.pid, 9)
 
-    # Request the module process to stop and release it 
     def stop_process(self):
+        """Request the module process to stop and release it"""
         if self.process:
-            logger.log("I'm stopping module '%s' process pid:%s " % (self.get_name(), self.process.pid))
+            logger.log("I'm stopping module '%s' process pid:%s " %
+                       (self.get_name(), self.process.pid))
             self.process.terminate()
             self.process.join(timeout=1)
             if self.process.is_alive():
@@ -185,25 +193,21 @@ class BaseModule(object):
                 self.__kill()
             self.process = None
 
-
-
     ## TODO: are these 2 methods really needed ?
     def get_name(self):
         return self.name
-    
-    # The classic has : do we have a prop or not?
+
     def has(self, prop):
+        """The classic has : do we have a prop or not ?"""
         return hasattr(self, prop)
 
-
-    # Request the module to manage the given brok.
-    # There a lot of different possible broks to manage.
     def manage_brok(self, brok):
-
+        """Request the module to manage the given brok.
+        There a lot of different possible broks to manage.
+        """
         manage = getattr(self, 'manage_' + brok.type + '_brok', None)
         if manage:
             return manage(brok)
-
 
     def manage_signal(self, sig, frame):
         self.interrupted = True
@@ -211,31 +215,33 @@ class BaseModule(object):
     def set_signal_handler(self, sigs=None):
         if sigs is None:
             sigs = (signal.SIGINT, signal.SIGTERM)
-            
+
         for sig in sigs:
             signal.signal(sig, self.manage_signal)
-    
+
     set_exit_handler = set_signal_handler
-    
-    # Called just before the module will exit
-    # Put in this method all you need to cleanly 
-    # release all open resources used by your module
-    def do_stop(self): 
+
+    def do_stop(self):
+        """Called just before the module will exit
+        Put in this method all you need to cleanly
+        release all open resources used by your module
+        """
         pass
-    
-    # For external modules only: implement in this method the body of you main loop
+
     def do_loop_turn(self):
+        """For external modules only:
+        implement in this method the body of you main loop
+        """
         raise NotImplementedError()
-    
-    # module "main" method. Only used by external modules.
+
     def main(self):
+        """module "main" method. Only used by external modules."""
         self.set_signal_handler()
         logger.log("[%s[%d]]: Now running.." % (self.name, os.getpid()))
         while not self.interrupted:
             self.do_loop_turn()
         self.do_stop()
         logger.log("[%s]: exiting now.." % (self.name))
-    
-    # TODO: apparently some modules would uses "work" as the main method ??    
-    work = main 
 
+    # TODO: apparently some modules would uses "work" as the main method ??
+    work = main
