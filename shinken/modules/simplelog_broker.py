47467e1e80e8e568b699b2dcbfcb060b6780524d
Guillaume Bour <guillaume.bour@uperto.com>
2012-04-06 11:04:17 +0200

    Cleanup: manage_brok() function already defined in parent class (BaseModule)

--- shinken/modules/simplelog_broker.py
+++ shinken/modules/simplelog_broker.py
@@ -124,9 +124,9 @@ class Simple_log_broker(BaseModule):
 
 
     def manage_brok(self, brok):
-
         """ Request the module to manage the given brok.
-There a lot of different possible broks to manage. """
+        There a lot of different possible broks to manage. 
+        """
         manage = getattr(self, 'manage_' + brok.type + '_brok', None)
         if manage:
             self.check_and_do_archive()
