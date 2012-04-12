b7e49601785f305faf85559767c83e9ec114ad45
Sebastien Coavoux <s.coavoux@free.fr>
2012-04-12 22:47:33 +0200

    Code Review. #99 Fixed : typo in comments Fixed : misplaced comments Fixed : PEP 8 style (check scripts are test/pylint_pass.sh and test/pep8_pass.sh) Updated : licence in files Added : # -*- coding: utf-8 -*- in header
    Personal Note :
    Pep 8 style is now frozen. I'll try to apply this to every files :)
    
    Feel free to (dis)agree

--- shinken/modules/couchdb_broker/__init__.py
+++ shinken/modules/couchdb_broker/__init__.py
@@ -1,42 +1,47 @@
 #!/usr/bin/python
-#Copyright (C) 2009 Gabes Jean, naparuba@gmail.com
+
+# Copyright (C) 2009-2011 :
+#    Gabes Jean, naparuba@gmail.com
+#    Gerhard Lausser, Gerhard.Lausser@consol.de
+#    Gregory Starck, g.starck@gmail.com
+#    Hartmut Goebel, h.goebel@goebel-consult.de
 #
-#This file is part of Shinken.
+# This file is part of Shinken.
 #
-#Shinken is free software: you can redistribute it and/or modify
-#it under the terms of the GNU Affero General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+# Shinken is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#Shinken is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU Affero General Public License for more details.
+# Shinken is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
 #
-#You should have received a copy of the GNU Affero General Public License
-#along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with Shinken.  If not, see <http://www.gnu.org/licenses/>.ses/>.
 
 
 properties = {
-    'daemons' : ['broker'],
-    'type' : 'couchdb',
-    'phases' : ['running'],
+    'daemons': ['broker'],
+    'type': 'couchdb',
+    'phases': ['running'],
     }
 
 
-#called by the plugin manager to get a broker
+# called by the plugin manager to get a broker
 def get_instance(plugin):
 
     print "Get a Couchdb broker for plugin %s" % plugin.get_name()
 
-    #first try the import
+    # first try the import
     try:
         from couchdb_broker import Couchdb_broker
-    except ImportError , exp:
-        print "Warning : the plugin type couchdb is unavalable : %s" % exp
+    except ImportError, exp:
+        print "Warning : the plugin type couchdb is unavailable : %s" % exp
         return None
 
-    #TODO : catch errors
+    # TODO : catch errors
     host = plugin.host
     user = plugin.user
     password = plugin.password
