0f983445b73f202f9fa65bf3b7ff683562747aaa
Sebastien Coavoux <s.coavoux@free.fr>
2012-04-13 23:43:12 +0200

    small fix of the big update :)

--- shinken/modules/service_perfdata_broker/__init__.py
+++ shinken/modules/service_perfdata_broker/__init__.py
@@ -21,8 +21,7 @@
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with Shinken.  If not, see <http://www.gnu.org/licenses/>. License
-#along with Shinken.  If not, see <http://www.gnu.org/licenses/>..
+# along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
 
 
 properties = {
