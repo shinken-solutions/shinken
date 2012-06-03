470d3ded58fcfa818c8592e8e82208d0c3214115
David GUENAULT <dguenault@monitoring-fr.org>
2012-06-03 13:47:13 +0200

    Prevision of a new approach to deploy nconf with zero post install configuration

---
+++ install.d/config.nconf/.file_accounts.php
@@ -0,0 +1,20 @@
+<?php
+/*
+#
+# User/Password file for simple authentication
+# To disable a user, rows can be commented out with "#"
+#
+# :: is the delimiter of the fields
+# Do NOT use :: (2 colons) in a data field (as name or password etc.) !
+#
+#username::password::authorization(user|admin)::[user's full name (optional)]::
+admin::nconf::admin::Administrator::
+#user::1234::user::Normal User::
+#dummy::9999::user::User Dummy::
+#foo::bar::user::foo bar::
+#
+# using encrypted passwords
+#user2::{CRYPT}s7FkIgzTWZia2::user::User with a CRYPT password::
+#
+*/
+?>
