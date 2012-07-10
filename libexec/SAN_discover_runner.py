19f8b3208908893bcb4b0badb38eed7fc09ce244
FORLOT Romain <rforlot@yahoo.com>
2012-07-10 10:08:44 +0200

    Finalize SAN discover runner script and a little bit of cleaning

--- libexec/SAN_discover_runner.py
+++ libexec/SAN_discover_runner.py
@@ -28,7 +28,6 @@
 ### modules import
 import optparse
 import re
-import sys
 import subprocess
 
 ##########
@@ -36,19 +35,28 @@ import subprocess
 ##########
 
 parser = optparse.OptionParser('%prog [options] -t targets') 
-cmd = "/opt/IBM_DS/client/SMcli"
+cmd = '/opt/IBM_DS/client/SMcli'
 
 # user name and password are defined in /var/lib/net-snmp/snmpd.conf
 # default parameters are defined in /usr/local/shinken/etc/resource.cfg
-parser.add_option("-t", "--targets", dest="targets", help="IP to manage")
+parser.add_option('-t', '--targets', dest='targets', help='comma separated IP list to manage')
+parser.add_option('-v', '--verbose', action='store_true', dest='verbose', help='be more verbose')
 
 
 opts, args = parser.parse_args()
 
 targets = opts.targets
+if opts.verbose:
+    verbose = True
+else:
+    verbose = False
+
+def debug(txt):
+    if verbose:
+        print txt
 
 if not opts.targets:
-    parser.error("Requires at least one ip (option -t)")
+    parser.error('Requires at least one ip (option -t)')
 
 
 ##############
@@ -56,35 +64,41 @@ if not opts.targets:
 ############## 
 
 ### Search for cluster software presents on targets
-def iptomanage(ip):
-    addip = [ cmd, '-A', ip ]
+def iptomanage(targets):
+    dict = {}
+    addip = [ cmd, '-A', targets ]
     sanname = [cmd, '-d', '-S']
-    adding = subprocess.Popen(addip, stdout=subprocess.PIPE)
+    adding = subprocess.Popen(' '.join(addip), stdout=subprocess.PIPE, shell=True)
 #    if adding.returncode != 0:
 #        print('Error when adding ip address into client software. Try manual adding to debug.')
 #        sys.exit(1)
+    debug(adding.communicate()[0])
     adding.wait()
     getmanageddev = subprocess.Popen(sanname,stdout=subprocess.PIPE)
     stdoutdata = getmanageddev.communicate()[0]
-    getsanname = re.match(r'(?P<sanname>\w+)\t.*'+ip, stdoutdata)
-    try:
-        name = getsanname.group('sanname')
-    except AttributeError:
-        print('Can not retrieve San name')
-        sys.exit(2)
-    return name
+    debug(stdoutdata)
+    for ip in targets.split(' '):
+        getsanname = re.search(r'(?P<sanname>\w+)\t.*'+ip, stdoutdata, flags=re.MULTILINE)
+        try:
+            sanname = getsanname.group('sanname')
+            try:
+                dict[sanname].append(ip)
+            except KeyError:
+                dict[sanname] = [ip]
+        except AttributeError:
+            print('Can not retrieve San name for '+ip)
+            continue
+    return dict
     
 
 ### converts the listed files systems writing and display them on the standard output
-def get_SAN_discovery_output(ip):
-    print "%s::_ctrl=%s"%(sanname, ip)# display like in the nmap model
+def get_SAN_discovery_output(dict):
+    for (sanname, ctrlIP) in dict.items():
+        print '%s::_ctrl=%s'%(sanname, ','.join(ctrlIP))
 
 ###############
 #     main    #
 ###############
 
-targets = targets.split(' ')
-for target in targets:
-    sanname = iptomanage(target)
-    get_SAN_discovery_output(target)
-
+res = iptomanage(targets)
+get_SAN_discovery_output(res)
