c7625ff0bcbed51e477ff5804dac81637b196150
FORLOT Romain <rforlot@yahoo.com>
2012-07-11 18:03:30 +0200

    Enh: SAN controllers hosts macros separated, 1 macros by controller

--- libexec/SAN_discover_runner.py
+++ libexec/SAN_discover_runner.py
@@ -29,77 +29,149 @@
 import optparse
 import re
 import subprocess
+import socket
+import fcntl
+import struct
+ 
+SIOCGIFNETMASK = 0x891b
+eth_dev_name='eth0'
 
 ##########
 #  menu  #
 ##########
 
 parser = optparse.OptionParser('%prog [options] -t targets') 
-cmd = '/opt/IBM_DS/client/SMcli'
+cmd = { 'ibm_ds' : '/opt/IBM_DS/client/SMcli',
+        'example' : '/path/to/cmd',
+      }
 
 # user name and password are defined in /var/lib/net-snmp/snmpd.conf
 # default parameters are defined in /usr/local/shinken/etc/resource.cfg
-parser.add_option('-t', '--targets', dest='targets', help='comma separated IP list to manage')
-parser.add_option('-v', '--verbose', action='store_true', dest='verbose', help='be more verbose')
+parser.add_option('-t', '--target', dest='target', help='IP to manage. One at a time only')
+parser.add_option('-v', '--vendor', dest='vendor', help='specify SAN vendor [ibm_ds|...]')
+parser.add_option('-n', '--network', action='store_true', dest='network', help='Take controller IP which are on same network as you are')
+parser.add_option('-d', '--debug', action='store_true', dest='debug', help='be more verbose')
 
 
 opts, args = parser.parse_args()
 
 targets = opts.targets
-if opts.verbose:
-    verbose = True
-else:
-    verbose = False
+vendor = opts.vendor
 
-def debug(txt):
-    if verbose:
+if opts.debug:
+    debug = True
+else:
+    debug = False
+
+def debuging(txt):
+    if debug:
         print txt
 
-if not opts.targets:
-    parser.error('Requires at least one ip (option -t)')
+if opts.network:
+    network = True
 
+if not opts.targets:
+    parser.error('Require at least one ip (option -t)')
+if not opts.vendor:
+    parser.error('Require SAN vendor name. [ibm_ds|...]')
+
+SANvendor = { 'ibm_ds' : { 'add_cmd' : [ cmd['ibm_ds'], '-A', target ],
+                           'getprofile_cmd' : [ cmd['ibm_ds'], target, '-c', 'show storagesubsystem profile;' ],
+                           'sanname_regex' :  re.compile('PROFILE FOR STORAGE SUBSYSTEM:\s(?P<sanname>\w+)\s+.*$', re.S|re.M),
+                           'controllers_ip_regex' : re.compile('IP address:\s+((?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))', re.S|re.M),
+                         },
+              'example' : { 'add_cmd' : [ cmd['example'], 'arg1', 'arg2' ],
+                            'getprofile_cmd' : [ cmd['example'], 'arg1', 'arg2' ],
+                            'sanname_regex' :  re.compile('(?P<sanname>\w+)', re.S|re.M),
+                            'controllers_ip_regex' : re.compile('IP address:\s+((?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))', re.S|re.M),
+                          },
+}
 
 ##############
 #  functions #
 ############## 
 
-### Search for cluster software presents on targets
-def iptomanage(targets):
-    dict = {}
-    addip = [ cmd, '-A', targets ]
-    sanname = [cmd, '-d', '-S']
-    adding = subprocess.Popen(' '.join(addip), stdout=subprocess.PIPE, shell=True)
-#    if adding.returncode != 0:
-#        print('Error when adding ip address into client software. Try manual adding to debug.')
-#        sys.exit(1)
-    debug(adding.communicate()[0])
-    adding.wait()
-    getmanageddev = subprocess.Popen(sanname,stdout=subprocess.PIPE)
-    stdoutdata = getmanageddev.communicate()[0]
-    debug(stdoutdata)
-    for ip in targets.split(' '):
-        getsanname = re.search(r'(?P<sanname>\w+)\t.*'+ip, stdoutdata, flags=re.MULTILINE)
-        try:
-            sanname = getsanname.group('sanname')
-            try:
-                dict[sanname].append(ip)
-            except KeyError:
-                dict[sanname] = [ip]
-        except AttributeError:
-            print('Can not retrieve San name for '+ip)
-            continue
-    return dict
-    
+### Code snippet to retrieve some system network informations
+def get_network_mask(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    netmask = fcntl.ioctl(s, SIOCGIFNETMASK, struct.pack('256s', ifname))[20:24]
+    return socket.inet_ntoa(netmask)
+ 
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
+def address_in_network(ip,net):
+   ipaddr = struct.unpack('L',socket.inet_aton(ip))[0]
+   netaddr,bits = net.split('/')
+   netmask = struct.unpack('L',socket.inet_aton(netaddr))[0] & struct.unpack('L',socket.inet_aton(bits))[0]
+   return ipaddr & netmask == netmask
+
+### Search for cluster software presents on targets
+def set_ip():
+    addip = v['add_cmd']
+    adding = subprocess.Popen(' '.join(addip), stdout=subprocess.PIPE, shell=True)
+    debuging(adding.communicate()[0])
+    adding.wait()
+    
+def get_SAN_profile():
+    sanprofile = v['getprofile_cmd']
+    get_managed_dev = subprocess.Popen(sanprofile,stdout=subprocess.PIPE)
+    stdoutdata = get_managed_dev.communicate()[0]
+    debuging(stdoutdata)
+    return stdoutdata
+
+def get_name(san_profile):
+    getsanname = v['sanname_regex'].search(san_profile)
+    try:
+        sanname = getsanname.group('sanname')
+    except AttributeError:
+        print('Can not retrieve San name')
+    return sanname
+
+def get_controllers_ip(san_profile, keep_on_same_network=False):
+    ctrl = v['controllers_ip_regex'].findall(san_profile)
+    debuging('Find ip : %s' % ctrl)
+    if keep_on_same_network:
+        my_ip = get_ip_address(eth_dev_name)
+        my_netmask = get_network_mask(eth_dev_name)
+        my_subnet_unpacked = struct.unpack('L', socket.inet_aton(my_ip))[0] & struct.unpack('L', socket.inet_aton(my_netmask))[0]
+        my_subnet = socket.inet_ntoa(struct.pack('L', my_subnet_unpacked))
+        n = [ my_subnet, my_netmask ]
+        i = 0
+        for ip in ctrl:
+            if not address_in_network(ip, '/'.join(n)):
+               ctrl.pop(i)
+            i += 1
+    return ctrl
+    
 ### converts the listed files systems writing and display them on the standard output
-def get_SAN_discovery_output(dict):
-    for (sanname, ctrlIP) in dict.items():
-        for ip in ctrlIP:
-            print '%s::_ctrl=%s'%(sanname,ip)
+def get_discovery_output(sanname, ctrlIP):
+    i = 1
+    for ip in ctrlIP:
+        print '%s::_ctrl%d=%s'%(sanname, i, ip)
+        i += 1
 
 ###############
 #     main    #
 ###############
 
-res = iptomanage(targets)
-get_SAN_discovery_output(res)
+v = SANvendor[vendor]
+
+# Add ip in client software managing SAN device
+set_ip()
+
+# Get SAN profile from client
+profile = get_SAN_profile()
+
+# Get SAN device name
+sanname = get_name(profile)
+
+# Get List of controllers IP
+ctrl_ip = get_controllers_ip(profile, network)
+
+get_discovery_output(sanname,ctrl_ip)
