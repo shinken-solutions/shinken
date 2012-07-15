f2c3a404f0ff10dde27c36d90aad90f2d69d6600
Hartmut Goebel <h.goebel@crazy-compilers.com>
2012-07-15 15:46:23 +0200

    Fix: The system load is nummeric, compare it as such, not as strings.
    Comparing numbers as strings may yield wrong results, e.g. '9' > '80' and
    '60' < '7'.
    
    Additionally remove some useless intermediate variables.

--- libexec/check_shinken_load.py
+++ libexec/check_shinken_load.py
@@ -16,26 +16,14 @@ if __name__ == "__main__":
     parser.add_argument('-w', '--warning', default='3,2,1')
     parser.add_argument('-c', '--critical', default='4,3,2')
     args = parser.parse_args()
-    critical = args.critical
-    warning = args.warning
 
-    (d1, d2, d3) = os.getloadavg()
+    critical = map(float, args.critical.split(','))
+    warning = map(float, args.warning.split(','))
 
-    load1 = str(d1)
-    load5 = str(d2)
-    load15 = str(d3)
+    (cload1, cload5, cload15) = critical
+    (wload1, wload5, wload15) = warning
 
-    listcritical = critical.split(',')
-    listwarning = warning.split(',')
-
-    cload1 = str(listcritical[0])
-    cload5 = str(listcritical[1])
-    cload15 = str(listcritical[2])
-
-    wload1 = str(listwarning[0])
-    wload5 = str(listwarning[1])
-    wload15 = str(listwarning[2])
-
+    (load1, load5, load15) = os.getloadavg()
 
     if load1 >= cload1 or load5 >= cload5 or load15 >= cload15:
         print ('CRITICAL - Load average : %s,%s,%s|load1=%s;load5=%s;load15=%s'
