58733f3847bf9549d4f653b03cbf31ffd594755d
Naparuba <naparuba@gmail.com>
2012-07-16 09:53:43 +0200

    Fix : (reported by : Sven) space in hostgroups problems. Fix #452

---
+++ test/test_hostgroup_with_space.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# Copyright (C) 2009-2010:
+#    Gabes Jean, naparuba@gmail.com
+#    Gerhard Lausser, Gerhard.Lausser@consol.de
+#
+# This file is part of Shinken.
+#
+# Shinken is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Shinken is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+# This file is used to test reading and processing of config files
+#
+
+from shinken_test import *
+
+
+class TestHostGroupWithSpace(ShinkenTest):
+
+    def setUp(self):
+        self.setup_with_file('etc/nagios_hostgroup_with_space.cfg')
+
+
+    def test_hostgroup_with_space(self):
+        svc = self.sched.services.find_srv_by_name_and_hostname("test_host_0", "test_With Spaces")
+        self.assert_(svc is not None)
+
+        svc = self.sched.services.find_srv_by_name_and_hostname("test_host_0", 'test_With anoter Spaces')
+        self.assert_(svc is not None)
+
+if __name__ == '__main__':
+    unittest.main()
