5d1e223fa859472a3f93f4e2531d1e1b2bfd5996
Hartmut Goebel <h.goebel@crazy-compilers.com>
2012-07-29 20:30:45 +0200

    Fix: `test.sh` did not run if called from within `test` directory.
    Solution: Get directory name from script's path, as it is done by the
    other scripts.

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-cd test
+DIR=$(cd $(dirname "$0"); pwd)
+cd $DIR
 echo "$PWD"
 #python test_problem_impact.py
 for ii in `ls -1 test_*py`; do echo "Launching Test $ii" && python $ii; done
