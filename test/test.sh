ebfe80e10601802d647ccf7cc36c51341b48e36f
Hartmut Goebel <h.goebel@crazy-compilers.com>
2012-07-29 20:33:55 +0200

    Minor clean-up to test.sh.
    - Reformat for-loop to not be squeezed into a single line.
    - Use `$(...)` instead of old-style backquote for command substitution.
    - Remove outdated comment.

--- test/test.sh
+++ test/test.sh
@@ -3,5 +3,8 @@
 DIR=$(cd $(dirname "$0"); pwd)
 cd $DIR
 echo "$PWD"
-#python test_problem_impact.py
-for ii in `ls -1 test_*py`; do echo "Launching Test $ii" && python $ii; done
+
+for ii in $(ls -1 test_*py) ; do
+    echo "Launching Test $ii"
+    python $ii
+done
