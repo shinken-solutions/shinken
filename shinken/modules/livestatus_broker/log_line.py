81f79392917b8d0a2e1ea9a95b0dbc3452d09f22
olivierHa <olivier.hanesse@gmail.com>
2013-03-11 16:31:17 +0100

    Dont log debug messages

--- shinken/modules/livestatus_broker/log_line.py
+++ shinken/modules/livestatus_broker/log_line.py
@@ -188,11 +188,12 @@ class Logline(dict):
                      type.startswith('Bailing out') or \
                      type.startswith('active mode...') or \
                      type.startswith('standby mode...') or \
-                     type.startswith('Debug') or \
                      type.startswith('Warning') or \
                      type.startswith('Info'):
                     logobject = LOGOBJECT_INFO
                     logclass = LOGCLASS_PROGRAM
+                elif type.startswith('Debug'):
+                    pass
                 else:
                     logger.debug("[Livestatus Log Lines] Does not match")
                     pass
