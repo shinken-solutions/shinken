50d14bc40ffc4480f45f205a9aacd1eeeda4604e
David GUENAULT <david.guenault@gmail.com>
2013-05-16 20:39:30 +0200

    working version of import_config.py

--- libexec/eue/import_config.py
+++ libexec/eue/import_config.py
@@ -67,13 +67,15 @@ try:
             "capture_level" : "scenario",
             "videocodec" : "libtheora -vsync 0 -r 24 -b 2496k -bt 1024k",
             "videoextension" : "ogg"
+        },
         "security" : {
             "hidepatterns" : "le mot de passe (\"[^\"]*\");the password (\"[^\"]*\")",
             "patternseparator" : ";",
         },
         "application" : {
             "name" : "Application GLPI",
-            "robot",
+        },
+        "robot" : {
             "env" : "prod",
             "name" : "poller-eue-spain",
             "os" : "linux",
@@ -84,15 +86,15 @@ try:
             "path" : "/usr/local/firefox/bin/firefox",
             "browser" : "firefox",
             "profile" : "auto",
-            "use_proxy" : "0",
+            "use_proxy" : 0,
             "proxy_host" : "",
-            "proxy_port" : "",
+            "proxy_port" : 0,
             "proxy_autourl" : "",
         },
         "execution" : {
             "mode" : "visible",
             "resolution" : "1280x1024x24",
-            "display" : "999",
+            "display" : 999,
         },
         "sikuli" : {
             "enabled" : "false",
@@ -101,15 +103,15 @@ try:
             "path" : "/opt/Sikuli-IDE",
             "project" : "cacoo.sikuli",
             "host" : "localhost",
-            "port" : "1339",
-            "timeout" : "60",
+            "port" : 1339,
+            "timeout" : 60,
         },
         "shinken" : {
             "base_uri" : "http://192.168.122.206:7767/eue_report",
         },
         "mongo" : {
             "host" : "localhost",
-            "port" : "27017",
+            "port" : 27017,
             "db" : "shinken",
             "collection" : "eue",
             "dbgrid" : "euemedia",
@@ -119,8 +121,17 @@ try:
         }
     }
 
-    config = ConfigParser.RawConfigParser(allow_no_value=True)
+    config = ConfigParser.RawConfigParser()
     config.read(inifile)
+    for section in config.sections():
+        print "[%s]" % (section)
+        for option,value in config.items(section):
+            print "  %s = %s" % (option,value)
+            data[section][option] = value
+
+    # now insert document in collection
+    col.insert(data)
+
 except:
     print "Unexpected error:", sys.exc_info()[0]
     print "host : %s" % (host)
