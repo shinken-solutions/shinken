7b968bcb347dad76afb1024b8eabf3339b84153e
Sébastien Coavoux <s.coavoux@free.fr>
2013-07-15 16:54:18 -0400

    Fix : Mixing bash and sh style

--- test/test_livestatus/test_mongodb.sh
+++ test/test_livestatus/test_mongodb.sh
@@ -2,17 +2,17 @@
 
 DIR=$(readlink -f $(dirname "$0")/..)
 
-if [ "$DIR" == "" ]; then
+if [ "$DIR" = "" ]; then
     echo "DIR MUST NOT BE EMPTY. I WONT TRY TO REMOVE /TMP"
     exit 2
 fi
 
-if [ "$1" == "start" ]; then
+if [ "$1" = "start" ]; then
     if [ ! -d "$DIR/tmp/mongodb" ]; then
         mkdir "$DIR/tmp/mongodb"
     fi
     mongod --dbpath $DIR/tmp/mongodb --smallfiles --pidfilepath $DIR/tmp/pid.txt --fork --logpath $DIR/tmp/log.txt & 
-elif [ "$1" == "stop" ]; then
+elif [ "$1" = "stop" ]; then
     kill $(cat $DIR/tmp/pid.txt)
     rm -rf $DIR/tmp/mongodb/* $DIR/tmp/pid.txt $DIR/tmp/log.txt
 else
