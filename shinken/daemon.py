685886ec9231b548cd19709ecb422aefab073d53
Jean Gabes <naparuba@gmail.com>
2013-07-26 18:09:49 +0200

    Fix: old multiprocessing lib where not initialized. Fix #950

--- shinken/daemon.py
+++ shinken/daemon.py
@@ -547,6 +547,7 @@ class Daemon(object):
             if len(startargs[0]) > 1:
                 self.manager.start(close_http_daemon, initargs=(self.http_daemon,))
             else:
+                self.manager.start()
                 logger.warning('Your multiprocessing librairy seems too old or strange, please use a vanilla python version instead')
             # Keep this daemon in the http_daemn module
         # Will be add to the modules manager later
