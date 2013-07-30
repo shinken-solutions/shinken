0df9eaca77fd7dae89152ab1bb60af6fdecef698
Sébastein Coavoux <s.coavoux@free.fr>
2013-07-29 22:04:16 -0400

    Test migration : Remove sqlitedb tests. naparuba/shinken#952

--- test/test_db_sqlite/create_db_sqlite.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd test_db_sqlite
-if [ ! -f test_db_sqlite.sqlite ];then 
-    sqlite3 test_db_sqlite.sqlite  < test_db_sqlite.sql
-fi
--- test/test_db_sqlite/test_db_sqlite.sql
+++
@@ -1,4 +0,0 @@
-create table service(name varchar(10), instance_id int);
-insert into service values('Shinken', 0);
-insert into service values('Is Fun', 1);
-
