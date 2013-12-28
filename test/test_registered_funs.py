567b7dde878722735abb16456253405ec343a2b2
Sébastein Coavoux <s.coavoux@free.fr>
2013-12-28 15:35:21 -0500

    Add test case for #1028

--- test/test_registered_funs.py
+++ test/test_registered_funs.py
@@ -22,44 +22,18 @@
 # This file is used to test reading and processing of config files
 #
 
-import os
-import tempfile
+
+import subprocess
+from time import sleep
 
 from shinken_test import *
 
 import shinken.log as shinken_log
 
-from shinken.daemons.pollerdaemon import Poller
-from shinken.daemons.brokerdaemon import Broker
 from shinken.daemons.schedulerdaemon import Shinken
-from shinken.daemons.reactionnerdaemon import Reactionner
 from shinken.daemons.arbiterdaemon import Arbiter
-try:
-    import pwd, grp
-    from pwd import getpwnam
-    from grp import getgrnam
-
-
-    def get_cur_user():
-        return pwd.getpwuid(os.getuid()).pw_name
-
-    def get_cur_group():
-        return grp.getgrgid(os.getgid()).gr_name
-
-except ImportError, exp:  # Like in nt system or Android
-    # temporary workaround:
-    def get_cur_user():
-        return os.getlogin()
-
-    def get_cur_group():
-        return os.getlogin()
-
-curdir = os.getcwd()
 
 daemons_config = {
-    Broker:       "etc/core/daemons/brokerd.ini",
-    Poller:       "etc/core/daemons/pollerd.ini",
-    Reactionner:  "etc/core/daemons/reactionnerd.ini",
     Shinken:      "etc/core/daemons/schedulerd.ini",
     Arbiter:    ["etc/core/shinken.cfg"]
 }
@@ -70,9 +44,8 @@ class testRegisteredFunctions(unittest.TestCase):
         cls = Shinken
         return cls(daemons_config[cls], False, True, False, None, '')
 
-
     def test_registered(self):
-        print "Testing register funs"
+        logger.info("Testing register funs")
 
         shinken_log.local_log = None  # otherwise get some "trashs" logs..
         d = self.create_daemon()
@@ -82,15 +55,32 @@ class testRegisteredFunctions(unittest.TestCase):
         d.http_backend = 'wsgiref'
         #d.port = HIGH_PORT + run  # random high port, I hope no one is using it :)
         d.do_daemon_init_and_start()
+        d.load_modules_manager()
         d.http_daemon.register(d.interface)
         reg_list = d.http_daemon.registered_fun
         expected_list = ['get_external_commands', 'get_running_id', 'got_conf', 'have_conf',
                          'ping', 'push_broks', 'push_host_names', 'put_conf', 'remove_from_conf',
                          'run_external_commands', 'set_log_level', 'wait_new_conf', 'what_i_managed']
-        assert( reg_list == expected_list)
+        for fun in expected_list:
+            assert(fun in reg_list)
+        subprocess.Popen(["../bin/shinken-arbiter", "-c", daemons_config[Arbiter][0], "-d"])
+        # Ok, now the conf
+        d.wait_for_initial_conf()
+        if not d.new_conf:
+            return
+        logger.info("New configuration received")
+        d.setup_new_conf()
+        reg_list = d.http_daemon.registered_fun
+        expected_list = ['get_external_commands', 'get_running_id', 'got_conf', 'have_conf',
+                         'ping', 'push_broks', 'push_host_names', 'put_conf', 'remove_from_conf',
+                         'run_external_commands', 'set_log_level', 'wait_new_conf', 'what_i_managed',
+                         'get_checks', 'put_results', 'fill_initial_broks', 'get_broks']
+        for fun in expected_list:
+            assert(fun in reg_list)
+
+        sleep(2)
+        os.kill(int(file("tmp/arbiterd.pid").read()), 2)
         d.do_stop()
-
-
 
 
 if __name__ == '__main__':
