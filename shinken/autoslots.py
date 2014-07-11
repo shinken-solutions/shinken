347a19b20397d6828136122540c3ad98ae4e8029
Sébastien Coavoux <s.coavoux@free.fr>
2014-07-11 13:50:52 -0400

    Enh : Update copyright year to 2014 in shinken directory

--- shinken/autoslots.py
+++ shinken/autoslots.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2009-2012:
+# Copyright (C) 2009-2014:
 #    Gabes Jean, naparuba@gmail.com
 #    Gerhard Lausser, Gerhard.Lausser@consol.de
 #    Gregory Starck, g.starck@gmail.com
--- shinken/objects/service.py
+++ shinken/objects/service.py
@@ -2,7 +2,7 @@
 
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2009-2012:
+# Copyright (C) 2009-2014:
 #    Gabes Jean, naparuba@gmail.com
 #    Gerhard Lausser, Gerhard.Lausser@consol.de
 #    Gregory Starck, g.starck@gmail.com
