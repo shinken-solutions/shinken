ac0becb7238915410aa95c53c02ec0bca405f3ee
Grégory Starck <gregory.starck@savoirfairelinux.com>
2014-11-14 09:56:20 -0500

    Py26: Still try to load importlib from system before fall back on our copied one.

--- shinken/misc/importlib.py
+++ shinken/misc/importlib.py
@@ -4,6 +4,9 @@ from __future__ import absolute_import
 import sys
 
 if sys.version_info[:2] <= (2, 6):
-    from ._importlib import *
+    try: # still try to import it from system:
+        from importlib import *
+    except ImportError:
+        from ._importlib import *
 else:
     from importlib import *
