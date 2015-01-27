f223104fc1256dedfdbb674fe1b56ad9799183f9
aviau <alexandre.viau@savoirfairelinux.com>
2015-01-26 21:57:31 -0500

    PEP8: old_daemon_link.py

--- shinken/old_daemon_link.py
+++ shinken/old_daemon_link.py
@@ -12,7 +12,9 @@ def make_deprecated_daemon_link(new_module):
     stack = inspect.stack()
     full_mod_name = stack[1][0].f_locals['__name__']
     mod_name = full_mod_name.split('.')[-1]
-    deprecation("{fullname} is deprecated module path ; "
-                "{name} must now be imported from shinken.objects.{name}"
-                " ; please update your code accordingly".format(name=mod_name, fullname=full_mod_name))
+    deprecation(
+        "{fullname} is deprecated module path ; "
+        "{name} must now be imported from shinken.objects.{name}"
+        " ; please update your code accordingly".format(name=mod_name, fullname=full_mod_name)
+    )
     sys.modules[full_mod_name] = new_module
