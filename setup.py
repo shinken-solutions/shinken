9d967fcde553362b359dd42696ca8722a5dc1426
Naparuba <naparuba@gmail.com>
2015-04-02 09:16:03 +0200

    Fix: at_exit error with pbr form a debian7 python bug

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 import os
+
+# Fix for debian 7 python that raise error on at_exit at the end of setup.py (cf http://bugs.python.org/issue15881)
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
 from shinken.bin import VERSION
 os.environ['PBR_VERSION'] = VERSION
 
