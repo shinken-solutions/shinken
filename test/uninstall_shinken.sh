f560364fd31706fa69bb1568ca1610e99a71d80b
Sébastien Coavoux <s.coavoux@free.fr>
2015-04-24 16:51:20 -0400

    Add uninstall call

--- test/uninstall_shinken.sh
+++ test/uninstall_shinken.sh
@@ -14,9 +14,17 @@ function remove_install_virtualenv(){
     find ~ -name "Shinken*.egg" -exec rm -rf {} \;
 }
 
+function remove_install_root(){
+    find /usr/ -name "Shinken*.egg" -exec rm -rf {} \;  # pip uninstall should do it
+    rm -rf /usr/bin/shinken* /var/lib/shinken /var/log/shinken/
+    rm -rf /run/shinken /etc/shinken /etc/default/shinken /etc/init.d/shinken*
+
+}
+
+
 if [[ "$#" -ne 2 ]]; then
     echo "Please specify install method and env [install|develop] [virtualenv|root]"
     echo "Example : $0 develop virtualenv"
 fi
 
-
+remove_${1}_${2}
