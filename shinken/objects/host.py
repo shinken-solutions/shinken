shinken-solutions/shinken

commit 9f9bc2040289bd897a9064c6445ebc08221440f9

Add: feature to make white list for services of an Host
Recheck for excluded/included services after all duplications.

Fix: Reworked Items. (add|remove)_item and related (un)index_item functions
+ simplified / clarified.
+ better docstrings.

Enh: simplify Service: dropped custom version of (un)indexitem()
There is now no need to redefine (un)indexitem() functions in Services.
Just use a name_property which is simply a (python-)property and return from it what we need.

Enh: Move inner function to class level.

Not really necessary to have it defined inner/Better to have it on the class level.

Fix: missed a continue
would AttributeError on next line otherwise.

Fix: adapted test_properties_defaults for new host property 'service_includes'.

Fix: adapted test : display_name defaults to instance's name_property
which now is (svc.host_name, svc.service_description) for a Service instance.

shinken/objects/host.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2009-2014:
@@ -54,6 +53,12 @@ class Host(SchedulingItem):
     id = 1  # zero is reserved for host (primary node for parents)
     ok_up = 'UP'
     my_type = 'host'
+
+    # if Host(or more generally Item) instances were created with all properties
+    # having a default value set in the instance then we wouldn't need this:
+    service_includes = service_excludes = []
+    # though, as these 2 attributes are to be relatively low used it's not
+    # that bad to have the default be defined only once here at the class level.
 
     # properties defined by configuration
     # *required: is required in conf
@@ -193,6 +198,8 @@ class Host(SchedulingItem):
         'service_overrides':
             ListProp(default=[], merging='duplicate', split_on_coma=False),
         'service_excludes':
+            ListProp(default=[], merging='duplicate', split_on_coma=True),
+        'service_includes':
             ListProp(default=[], merging='duplicate', split_on_coma=True),
         'labels':
             StringProp(default=[], fill_brok=['full_status'], merging='join', split_on_coma=True),
@@ -873,6 +880,31 @@ class Host(SchedulingItem):
     # Add one of our service to services (at linkify)
     def add_service_link(self, service):
         self.services.append(service)
+
+
+    def __repr__(self):
+        return '<Host host_name=%r name=%r use=%r />' % (
+            getattr(self, 'host_name', None),
+            getattr(self, 'name', None),
+            getattr(self, 'use', None))
+
+    __str__ = __repr__
+
+    def is_excluded_for(self, svc):
+        ''' Check whether this host should have the passed service be "excluded" or "not included".
+
+        An host can define service_includes and/or service_excludes directive to either
+        white-list-only or black-list some services from itself.
+
+        :type svc: shinken.objects.service.Service
+        '''
+
+        sdescr = svc.service_description
+        if not svc.is_tpl() and self.service_includes:
+            return sdescr not in self.service_includes
+        if self.service_excludes:
+            return sdescr in self.service_excludes
+        return False
 
 #####
 #                         _

shinken/objects/item.py
@@ -820,7 +820,7 @@ class Items(object):
 
     def remove_template(self, tpl):
         """
-        Removes and unindexes a template from the `templates` container.
+        Removes and unindex a template from the `templates` container.
 
         :param tpl: The template to remove
         """
@@ -843,9 +843,7 @@ class Items(object):
             pass
 
     def add_item(self, item, index=True):
-        """
-        Adds a template into the `items` container, and index it depending
-        on the `index` flag.
+        """Adds an item into our containers, and index it depending on the `index` flag.
 
         :param item:    The item to add
         :param index:   Flag indicating if the item should be indexed
@@ -855,24 +853,39 @@ class Items(object):
             item = self.index_item(item)
         self.items[item.id] = item
 
-    def index_item(self, item, name=None):
+    def remove_item(self, item):
+        """Removes (and un-index) an item from our containers.
+
+        :param item: The item to be removed.
+        :type item:  Item  # or subclass of
         """
-        Indexes a template by `name` into the `name_to_template` dictionnary.
+        self.unindex_item(item)
+        self.items.pop(item.id, None)
 
-        If an object holding the same name already exists in the index, the
-        conflict is managed by the `manage_conflict` method.
-
-        An optional `name` may be given to force the name under which the
-        object should be indexed.
+    def index_item(self, item):
+        """ Indexes an item into our `name_to_item` dictionary.
+        If an object holding the same item's name/key already exists in the index
+        then the conflict is managed by the `manage_conflict` method.
 
         :param item: The item to index
         :param name: The optional name to use to index the item
         """
+        # TODO: simplify this function (along with its opposite: unindex_item)
+        # it's too complex for what it does.
+        # more over:
+        # There are cases (in unindex_item) where some item is tried to be removed
+        # from name_to_item while it's not present in it !
+        # so either it wasn't added or it was added with another key or the item key changed
+        # between the index and unindex calls..
+        #  -> We should simply not have to call unindex_item() with a non-indexed item !
         name_property = getattr(self.__class__, "name_property", None)
-        objcls = self.inner_class.my_type
-        if name is None and name_property:
-            name = getattr(item, name_property, '')
+        # if there is no 'name_property' set(it is None), then the following getattr() will
+        # "hopefully" evaluates to '',
+        # unless some(thing|one) have setattr(item, None, 'with_something'),
+        # which would be rather odd :
+        name = getattr(item, name_property, '')
         if not name:
+            objcls = self.inner_class.my_type
             mesg = "a %s item has been defined without %s%s" % \
                    (objcls, name_property, self.get_source(item))
             item.configuration_errors.append(mesg)
@@ -881,37 +894,14 @@ class Items(object):
         self.name_to_item[name] = item
         return item
 
-    def remove_item(self, item):
-        """
-        Removes and unindexes an item from the `items` container.
-
-        :param item: The item to remove
-        """
-        name_property = getattr(self.__class__, "name_property", None)
-        if name_property:
-            self.unindex_item(item)
-        try:
-            del self.items[item.id]
-        except KeyError:
-            pass
-
-    def unindex_item(self, item, name=None):
-        """
-        Unindexes an item from the `items` container.
-
-        An optional `name` may be given to indicate the mame under which the
-        object was indexed.
-
+    def unindex_item(self, item):
+        """ Unindex an item from our name_to_item dict.
         :param item:    The item to unindex
-        :param name:    The name under which the item has been indexed.
         """
         name_property = getattr(self.__class__, "name_property", None)
-        if name is None and name_property:
-            name = getattr(item, name_property, '')
-        try:
-            del self.name_to_item[name]
-        except KeyError:
-            pass
+        if name_property is None:
+            return
+        self.name_to_item.pop(getattr(item, name_property, ''), None)
 
     def __iter__(self):
         return self.items.itervalues()

shinken/objects/service.py
@@ -543,6 +543,19 @@ class Service(SchedulingItem):
 #                        |___/
 ######
 
+    def __repr__(self):
+        return '<Service host_name=%r desc=%r name=%r use=%r />' % (
+            getattr(self, 'host_name', None),
+            getattr(self, 'service_description', None),
+            getattr(self, 'name', None),
+            getattr(self, 'use', None)
+        )
+    __str__ = __repr__
+
+    @property
+    def unique_key(self):  # actually only used for (un)indexitem() via name_property..
+        return (self.host_name, self.service_description)
+
     # Give a nice name output
     def get_name(self):
         if hasattr(self, 'service_description'):
@@ -721,17 +734,18 @@ class Service(SchedulingItem):
         # And the parent/child dep lists too
         srv.register_son_in_parent_child_dependencies(self)
 
-    # For a given host, look for all copy we must
-    # create for for_each property
     def duplicate(self, host):
-        duplicates = []
+        ''' For a given host, look for all copy we must create for for_each property
+        :type host: shinken.objects.host.Host
+        :return Service
+        '''
 
         # In macro, it's all in UPPER case
         prop = self.duplicate_foreach.strip().upper()
-
-        # If I do not have the property, we bail out
-        if prop not in host.customs:
+        if prop not in host.customs:  # If I do not have the property, we bail out
             return []
+
+        duplicates = []
 
         # Get the list entry, and the not one if there is one
         entry = host.customs[prop]
@@ -1227,6 +1241,8 @@ class Service(SchedulingItem):
 
 # Class for list of services. It's mainly, mainly for configuration part
 class Services(Items):
+
+    name_property = 'unique_key'  # only used by (un)indexitem (via 'name_property')
     inner_class = Service  # use for know what is in items
 
     def add_template(self, tpl):
@@ -1279,35 +1295,6 @@ class Services(Items):
         if index is True:
             item = self.index_item(item)
         self.items[item.id] = item
-
-    def index_item(self, item):
-        """
-        Indexes a template by `name` into the `name_to_template` dictionnary.
-
-        This implementation takes into account that a service has two naming
-        attribute: `host_name` and `service_description`.
-
-        :param item: The item to index
-        """
-        hname = getattr(item, 'host_name', '')
-        sdescr = getattr(item, 'service_description', '')
-        key = (hname, sdescr)
-        return Items.index_item(self, item, key)
-
-    def unindex_item(self, item):
-        """
-        Unindexes an item from the `items` container.
-
-        This implementation takes into account that a service has two naming
-        attribute: `host_name` and `service_description`.
-
-        :param item:    The item to unindex
-        :param name:    The name under which the item has been indexed.
-        """
-        host_name = getattr(item, 'host_name', '')
-        sdescr = getattr(item, 'service_description', '')
-        key = (host_name, sdescr)
-        Items.unindex_item(self, item, key)
 
     # Search for all of the services in a host
     def find_srvs_by_hostname(self, host_name):
@@ -1496,12 +1483,6 @@ class Services(Items):
         for sid in to_del:
             del self.items[sid]
 
-    def is_excluded_for_host(self, host, service):
-        sdescr = getattr(service, "service_description", '')
-        excludes = getattr(host, "service_excludes", '')
-        if not isinstance(excludes, list):
-            excludes = [e.strip() for e in excludes.split(',') if e.strip()]
-        return sdescr in excludes
 
     def explode_services_from_hosts(self, hosts, s, hnames):
         """
@@ -1541,45 +1522,61 @@ class Services(Items):
                 err = 'Error: The hostname %s is unknown for the ' \
                       'service %s!' % (hname, s.get_name())
                 s.configuration_errors.append(err)
-            if self.is_excluded_for_host(h, s):
+                continue
+            if h.is_excluded_for(s):
                 continue
             new_s = s.copy()
             new_s.host_name = hname
             self.add_item(new_s)
 
-    def explode_services_from_templates(self, hosts, t):
+    def _local_create_service(self, hosts, host_name, service):
+        '''Create a new service based on a host_name and service instance.
+
+        :param hosts:       The hosts items instance.
+        :type hosts:        shinken.objects.host.Hosts
+        :param host_name:   The host_name to create a new service.
+        :param service:     The service to be used as template.
+        :type service:      Service
+        :return:            The new service created.
+        :rtype:             Service
+        '''
+        h = hosts.find_by_name(host_name.strip())
+        if h.is_excluded_for(service):
+            return
+        # Creates concrete instance
+        new_s = service.copy()
+        new_s.host_name = host_name
+        new_s.register = 1
+        self.add_item(new_s)
+        return new_s
+
+
+    def explode_services_from_templates(self, hosts, service):
         """
         Explodes services from templates. All hosts holding the specified
         templates are bound the service.
 
-        :param hosts:   The hosts container
-        :param s:       The service to explode
+        :param hosts:   The hosts container.
+        :type hosts:    shinken.objects.host.Hosts
+        :param service: The service to explode.
+        :type service:  Service
         """
-        hname = getattr(t, "host_name", None)
-        if hname is None:
+        hname = getattr(service, "host_name", None)
+        if not hname:
             return
-
-        def local_create_service(name):
-            h = hosts.find_by_name(name.strip())
-            if self.is_excluded_for_host(h, t):
-                return
-            # Creates concrete instance
-            new_s = t.copy()
-            new_s.host_name = name
-            new_s.register = 1
-            self.add_item(new_s)
 
         # Now really create the services
         if is_complex_expr(hname):
             hnames = self.evaluate_hostgroup_expression(
                 hname.strip(), hosts, hosts.templates, look_in='templates')
             for name in hnames:
-                local_create_service(name)
+                self._local_create_service(hosts, name, service)
         else:
             hnames = [n.strip() for n in hname.split(',') if n.strip()]
             for hname in hnames:
                 for name in hosts.find_hosts_that_use_template(hname):
-                    local_create_service(name)
+                    self._local_create_service(hosts, name, service)
+
 
     def explode_services_duplicates(self, hosts, s):
         """
@@ -1587,7 +1584,9 @@ class Services(Items):
 
         :param hosts:   The hosts container
         :param s:       The service to explode
+        :type s:        Service
         """
+
         hname = getattr(s, "host_name", None)
         if hname is None:
             return
@@ -1604,7 +1603,7 @@ class Services(Items):
 
         # Duplicate services
         for new_s in s.duplicate(h):
-            if self.is_excluded_for_host(h, new_s):
+            if h.is_excluded_for(new_s):
                 continue
             # Adds concrete instance
             self.add_item(new_s)
@@ -1714,6 +1713,14 @@ class Services(Items):
             self.explode_services_duplicates(hosts, s)
             if not s.configuration_errors:
                 self.remove_item(s)
+
+        to_remove = []
+        for service in self:
+            host = hosts.find_by_name(service.host_name)
+            if host and host.is_excluded_for(service):
+                to_remove.append(service)
+        for service in to_remove:
+            self.remove_item(service)
 
         # Servicegroups property need to be fullfill for got the informations
         # And then just register to this service_group

test/test_properties_defaults.py
@@ -581,6 +581,7 @@ class TestHost(PropertiesTester, ShinkenTest):
         ('custom_views', []),
         ('service_overrides', []),
         ('service_excludes', []),
+        ('service_includes', []),
         ('business_rule_output_template', ''),
         ('business_rule_smart_notifications', False),
         ('business_rule_downtime_as_ack', False),

test/test_services.py
@@ -110,7 +110,7 @@ class TestService(ShinkenTest):
         svc = self.get_svc()
         print 'Display name', svc.display_name, 'toto'
         print 'Full name', svc.get_full_name()
-        self.assertEqual('test_ok_0', svc.display_name)
+        self.assertEqual((u'test_host_0', u'test_ok_0'), svc.display_name)
 
     def test_states_from_exit_status(self):
         svc = self.get_svc()

commit 2e218e4de45026db6f14eed487e78f42b0f04d31

Fixed service_(includes|excludes) tmpl recursion
Fixed a regression introduced in
9f9bc2040289bd897a9064c6445ebc08221440f9

If an attrubute covered by a property is explicitely set at class or
instance level (during initialization for example), it breaks
attribute's template recursion in `get_property_by_inheritance` on
`if hasattr(self, prop)` (attributes is always seen as affected, even if
it was never set).

In the present case, it broke service exclusion defined on templates
rather than host.

shinken/objects/host.py
@@ -54,12 +54,6 @@ class Host(SchedulingItem):
     ok_up = 'UP'
     my_type = 'host'
 
-    # if Host(or more generally Item) instances were created with all properties
-    # having a default value set in the instance then we wouldn't need this:
-    service_includes = service_excludes = []
-    # though, as these 2 attributes are to be relatively low used it's not
-    # that bad to have the default be defined only once here at the class level.
-
     # properties defined by configuration
     # *required: is required in conf
     # *default: default value if no set in conf
@@ -899,9 +893,9 @@ class Host(SchedulingItem):
         ''' Check whether this host should have the passed service *description*
             be "excluded" or "not included".
         '''
-        if not is_tpl and self.service_includes:
+        if not is_tpl and hasattr(self, "service_includes"):
             return sdesc not in self.service_includes
-        if self.service_excludes:
+        if hasattr(self, "service_excludes"):
             return sdesc in self.service_excludes
         return False
 