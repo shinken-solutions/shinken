53bc16c5bfb6292620c1b69089748762901f2c49
Alex Monk <krenair@gmail.com>
2015-10-13 04:17:31 +0100

    WebUI: Check X-Forwarded-Proto header when determining URL scheme
    Otherwise it always redirects to http when behind a proxy properly setting X-Forwarded-Proto
    
    Fixes https://phabricator.wikimedia.org/T85326

--- shinken/webui/bottlewebui.py
+++ shinken/webui/bottlewebui.py
@@ -994,7 +994,7 @@ class BaseRequest(DictMixin):
             but the fragment is always empty because it is not visible to the
             server. '''
         env = self.environ
-        http = env.get('wsgi.url_scheme', 'http')
+        http = env.get('HTTP_X_FORWARDED_PROTO') or env.get('wsgi.url_scheme', 'http')
         host = env.get('HTTP_X_FORWARDED_HOST') or env.get('HTTP_HOST')
         if not host:
             # HTTP 1.1 requires a Host-header. This is for HTTP/1.0 clients.
