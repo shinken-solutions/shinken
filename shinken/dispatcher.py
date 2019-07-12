9ba8cc9c97e6c4cdffeba44d36c3826c9c491942
Jean Gabès <naparuba@gmail.com>
2019-07-12 16:19:26 +0200

    Fix: TEST python unitary tests : still less pep8 errors, should be ok now.

--- shinken/dispatcher.py
+++ shinken/dispatcher.py
@@ -224,7 +224,7 @@ class Dispatcher:
                                 continue
                             if not satellite.alive or (
                                     satellite.reachable and not
-                            satellite.do_i_manage(cfg_id, push_flavor)):
+                                    satellite.do_i_manage(cfg_id, push_flavor)):
                                 logger.warning('[%s] The %s %s seems to be down, '
                                                'I must re-dispatch its role to someone else.',
                                                r.get_name(), kind, satellite.get_name())
